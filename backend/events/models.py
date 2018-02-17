ffaedc4d19faf9d45562932fe0cdc646f190de52
ydidwania <didwaniayashvardhan+github@gmail.com>
2018-02-18 03:34:24 +0530

    change to urlfield

--- backend/events/models.py
+++ backend/events/models.py
@@ -5,7 +5,7 @@ class Event(models.Model):
     id = models.UUIDField(primary_key=True)
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
-    image_url = models.CharField(max_length=100)
+    image_url = models.URLField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     # created_by : User
