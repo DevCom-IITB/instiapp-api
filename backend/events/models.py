aac2dfd7acc60e0337ebe3d285487ad385291971
ydidwania <didwaniayashvardhan+github@gmail.com>
2018-02-18 03:17:38 +0530

    changed id to uuid field

--- backend/events/models.py
+++ backend/events/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Event(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True)
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
     image_url = models.CharField(max_length=100)
