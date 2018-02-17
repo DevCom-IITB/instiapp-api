f95ccf83f940f7b29a9b09ce3732a7726e5e069e
Sajal Narang <sajalnarang@gmail.com>
2018-02-18 02:12:18 +0530

    Add events app

---
+++ backend/events/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Event(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    image_url = models.CharField(max_length=100)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    //created_by : User
+    all_day = models.BooleanField(default=False)
+    //venue : Venue
+    //body : Body
