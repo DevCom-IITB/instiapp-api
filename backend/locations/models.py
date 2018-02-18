975f0a4805637780f85efd5bf234062fadd6ef2b
RadialApps <radialapps@gmail.com>
2018-02-18 23:21:07 +0530

    Use DecimalField for latitude and longitude in Location Recommended by https://stackoverflow.com/questions/30706799/which-model-field-to-use-in-django-to-store-longitude-and-latitude-values

--- backend/locations/models.py
+++ backend/locations/models.py
@@ -5,8 +5,8 @@ class Location(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     time_of_creation = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=50)
-    lat = models.CharField(max_length=50)
-    lng = models.CharField(max_length=50)
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    lng = models.DecimalField(max_digits=9, decimal_places=6)
 
     def __str__(self):
         return self.name
