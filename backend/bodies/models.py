9a1c24233fc8104043c05d0a7b736bccbab77c6d
Varun Patil <radialapps@gmail.com>
2018-03-02 17:47:58 +0530

    Add models and migrations for children functionality

--- backend/bodies/models.py
+++ backend/bodies/models.py
@@ -1,4 +1,4 @@
-' Model for Body '
+' Model for Body and children'
 from uuid import uuid4
 from django.db import models
 
@@ -9,3 +9,10 @@ class Body(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
     image_url = models.URLField()
+
+class BodyChildRelation(models.Model):
+    ' Relates a body to one child '
+
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    parent = models.ForeignKey(Body, on_delete=models.CASCADE, default=uuid4, related_name='parent')
+    child = models.ForeignKey(Body, on_delete=models.CASCADE, default=uuid4, related_name='child')
