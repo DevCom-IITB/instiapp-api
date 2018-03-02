5b4733a177c363c29cc16d8fd6f64b69ef66ccc4
Varun Patil <radialapps@gmail.com>
2018-03-02 17:26:41 +0530

    Add initial bodies model

--- backend/bodies/models.py
+++ backend/bodies/models.py
@@ -1,3 +1,11 @@
+' Model for Body '
+from uuid import uuid4
 from django.db import models
 
-# Create your models here.
+class Body(models.Model):
+    ' An organization or club which may conduct events '
+
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    image_url = models.URLField()
