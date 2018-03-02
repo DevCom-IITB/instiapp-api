805e1be3babe1738ffee81b4748a4ab6092b8231
Varun Patil <radialapps@gmail.com>
2018-03-03 01:47:09 +0530

    Add links of parent bodies (child - of) to body

--- backend/bodies/serializers.py
+++ backend/bodies/serializers.py
@@ -1,9 +1,21 @@
 ' Serializers for Body and BodyChildRelation '
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 from bodies.models import Body, BodyChildRelation
 
+class ParentBodyHyperlink(serializers.HyperlinkedRelatedField):
+    ' Gets hyperlinks for parent bodies from BodyChildRelation'
+    view_name = 'body-detail'
+
+    def get_url(self, obj, view_name, request, format):
+        url_kwargs = {'pk': obj.parent.pk}
+        return reverse(view_name, kwargs=url_kwargs, request=request, format=format)
+
 class BodySerializer(serializers.HyperlinkedModelSerializer):
-    'Serialize Body'
+    'Serializer for Body'
+
+    child = ParentBodyHyperlink(many=True, read_only=True)
+
     class Meta:
         model = Body
-        fields = ('url', 'id', 'name', 'description', 'image_url')
+        fields = ('url', 'id', 'name', 'description', 'image_url', 'child')
