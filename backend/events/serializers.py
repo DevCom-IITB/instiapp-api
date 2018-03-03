52ba3859613627b529b2d449d6fc91a9d3dddb87
Varun Patil <radialapps@gmail.com>
2018-03-03 23:55:02 +0530

    Return only count of followers with Event Returning the user details with every event is unnecessary. If a single event is requested, it should return all followers (TODO)

--- backend/events/serializers.py
+++ backend/events/serializers.py
@@ -6,10 +6,19 @@ from locations.serializers import LocationSerializerMin
 class EventSerializer(serializers.HyperlinkedModelSerializer):
     'Serializer for Event'
 
+    interested_count = serializers.SerializerMethodField()
+    going_count = serializers.SerializerMethodField()
+
     venues = LocationSerializerMin(many=True, read_only=True)
 
     class Meta:
         model = Event
         fields = ('url', 'id', 'name', 'description', 'image_url',
                   'start_time', 'end_time', 'all_day', 'venues', 'bodies',
-                  'followers')
+                  'interested_count', 'going_count')
+
+    def get_interested_count(self, obj):
+        return obj.user_event_statuses.filter(status=1).count()
+
+    def get_going_count(self, obj):
+        return obj.user_event_statuses.filter(status=2).count()
