95184b45315d60beee9ab113bdfb52ab7e3e4a11
Varun Patil <radialapps@gmail.com>
2018-03-04 00:24:06 +0530

    Return followers when querying a single event

--- backend/events/serializers.py
+++ backend/events/serializers.py
@@ -1,13 +1,26 @@
 ' Serializers for Event '
 from rest_framework import serializers
 from events.models import Event
-from locations.serializers import LocationSerializerMin
+from locations.serializers import LocationSerializerMin, LocationSerializer
+from users.serializers import UserProfileSerializer
+
+class FollowersMethods:
+    ' Methods for followers '
+    def get_count(self, obj, s):
+        return obj.user_event_statuses.filter(status=s).count()
+
+    def get_followers(self, obj, s):
+        return [UserProfileSerializer(x.user, context=self.context).data \
+                for x in obj.user_event_statuses.filter(status=s)]
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
     'Serializer for Event'
 
     interested_count = serializers.SerializerMethodField()
+    get_interested_count = lambda self, obj: FollowersMethods.get_count(self, obj, 1)
+
     going_count = serializers.SerializerMethodField()
+    get_going_count = lambda self, obj: FollowersMethods.get_count(self, obj, 2)
 
     venues = LocationSerializerMin(many=True, read_only=True)
 
@@ -17,8 +30,25 @@ class EventSerializer(serializers.HyperlinkedModelSerializer):
                   'start_time', 'end_time', 'all_day', 'venues', 'bodies',
                   'interested_count', 'going_count')
 
-    def get_interested_count(self, obj):
-        return obj.user_event_statuses.filter(status=1).count()
+class EventFullSerializer(serializers.HyperlinkedModelSerializer):
+    'Serializer for Event with more information (followers, venues etc.)'
 
-    def get_going_count(self, obj):
-        return obj.user_event_statuses.filter(status=2).count()
+    interested_count = serializers.SerializerMethodField()
+    get_interested_count = lambda self, obj: FollowersMethods.get_count(self, obj, 1)
+
+    going_count = serializers.SerializerMethodField()
+    get_going_count = lambda self, obj: FollowersMethods.get_count(self, obj, 2)
+
+    interested = serializers.SerializerMethodField()
+    get_interested = lambda self, obj: FollowersMethods.get_followers(self, obj, 1)
+
+    going = serializers.SerializerMethodField()
+    get_going = lambda self, obj: FollowersMethods.get_followers(self, obj, 2)
+
+    venues = LocationSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Event
+        fields = ('url', 'id', 'name', 'description', 'image_url',
+                  'start_time', 'end_time', 'all_day', 'venues', 'bodies',
+                  'interested_count', 'going_count', 'interested', 'going')
--- backend/events/views.py
+++ backend/events/views.py
@@ -1,12 +1,12 @@
 ' Views for events app '
 from rest_framework import viewsets
-from events.serializers import EventSerializer
+from events.serializers import EventFullSerializer
 from events.models import Event
 
 class EventViewSet(viewsets.ModelViewSet):   # pylint: disable=too-many-ancestors
     ' API endpoint that allows events to be viewed or edited '
     queryset = Event.objects.all()
-    serializer_class = EventSerializer
+    serializer_class = EventFullSerializer
 
     def get_serializer_context(self):
         return {'request': self.request}
