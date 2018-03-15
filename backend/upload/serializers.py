bf61c0d0f9c55eb76cdc31e548468f53265dd3c7
Varun Patil <radialapps@gmail.com>
2018-03-16 00:14:46 +0530

    Change image upload endpoint to use base64

--- backend/upload/serializers.py
+++ backend/upload/serializers.py
@@ -1,9 +1,12 @@
 """Serializer for Image Uploads."""
 from rest_framework import serializers
+from drf_extra_fields.fields import Base64ImageField
 from upload.models import UploadedImage
 
 class UploadedImageSerializer(serializers.ModelSerializer):
     """Serializer for UploadedImage."""
+
+    picture = Base64ImageField()
 
     class Meta:
         model = UploadedImage
