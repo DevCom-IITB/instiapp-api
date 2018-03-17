ce134c7e99ac050a05e6ddc25c7092c585878648
Varun Patil <radialapps@gmail.com>
2018-03-17 18:35:34 +0530

    Lint, document

--- backend/login/views.py
+++ backend/login/views.py
@@ -1,4 +1,4 @@
-"""Login Viewset"""
+"""Login Viewset."""
 import requests
 from rest_framework import viewsets
 from rest_framework.response import Response
@@ -7,13 +7,23 @@ from django.contrib.auth.models import User
 from users.models import UserProfile
 from users.serializers import UserProfileFullSerializer
 
-class LoginViewSet(viewsets.ViewSet): 
-    def login(self, request):
+class LoginViewSet(viewsets.ViewSet):
+    """Viewset to handle logging in and out, and getting the current user's profile."""
+
+    @classmethod
+    def login(cls, request):
+        """Logs in the user using the {?code} query parameter"""
+
+        # Check if we have the auth code
         auth_code = request.GET.get('code')
         if auth_code is None:
             return HttpResponse(status=400, content="{?code} is required")
-        post_data = 'code=' + auth_code  + '&redirect_uri=http://localhost:8000/go&grant_type=authorization_code'
 
+        # Construt post data to get token
+        # pylint: disable=C0301
+        post_data = 'code=' + auth_code + '&redirect_uri=http://localhost:8000/go&grant_type=authorization_code'
+
+        # Get our access token
         response = requests.post(
             'https://gymkhana.iitb.ac.in/sso/oauth/token/',
             data=post_data,
@@ -23,12 +33,14 @@ class LoginViewSet(viewsets.ViewSet):
             })
         response_json = response.json()
 
+        # Check that we have the access token
         if not 'access_token' in response_json:
             print(response.content)
             if 'error' in response_json:
                 return HttpResponse(status=400, content=response_json['error'])
             return HttpResponse(status=400, content='Getting auth token failed')
 
+        # Get the user's profile
         profile_response = requests.get(
             'https://gymkhana.iitb.ac.in/sso/user/api/user/?fields=first_name,last_name,type,profile_picture,sex,username,email,program,contacts,insti_address,secondary_emails,mobile,roll_number',
             headers={
@@ -36,8 +48,10 @@ class LoginViewSet(viewsets.ViewSet):
             })
         profile_json = profile_response.json()
 
+        # Print the profile for debugging
         print(profile_response.content)
 
+        # Check if we got at least the user's SSO id
         if not 'id' in profile_json:
             if 'error' in profile_response:
                 return HttpResponse(status=400, content=profile_response['error'])
@@ -45,16 +59,19 @@ class LoginViewSet(viewsets.ViewSet):
 
         username = str(profile_json['id'])
 
+        # Check if the User exists, otherwise create a new object
         try:
             user = User.objects.get(username=username)
         except User.DoesNotExist:
             user = User.objects.create_user(username)
 
+        # Check if User has a profile and create if not
         try:
             user_profile = UserProfile.objects.get(user=user)
         except UserProfile.DoesNotExist:
             user_profile = UserProfile.objects.create(user=user, name='iitbuser')
 
+        # Fill in data into the profile from SSO
         for response_key, data_key in {
                 'first_name': 'name',
                 'email': 'email',
@@ -64,6 +81,7 @@ class LoginViewSet(viewsets.ViewSet):
             if response_key in profile_json:
                 setattr(user_profile, data_key, profile_json[response_key])
 
+        # Fill in some special parameters
         if 'first_name' in profile_json and 'last_name' in profile_json:
             user_profile.name = profile_json['first_name'] + ' ' + profile_json['last_name']
 
@@ -73,28 +91,37 @@ class LoginViewSet(viewsets.ViewSet):
         if 'profile_picture' in profile_json:
             user_profile.profile_pic = 'https://gymkhana.iitb.ac.in' + profile_json['profile_picture']
 
+        # Save the profile
         user_profile.save()
 
+        # Log in the user
         request.session['username'] = username
 
+        # Return the session id
         return Response({
             'sessionid': request.session.session_key,
             'user': user.username,
             'profile_id': user_profile.id
         })
 
-    def get_user(self, request):
+    @classmethod
+    def get_user(cls, request):
+        """Get the session and user's profile."""
+
+        # Check if the user is logged in
         if 'username' in request.session:
             username = request.session['username']
         else:
             return HttpResponse(status=401, content="Not logged in")
 
+        # Check if the user has a profile
         try:
             user_profile = UserProfile.objects.get(user__username=username)
             profile_serialized = UserProfileFullSerializer(user_profile)
         except UserProfile.DoesNotExist:
             return HttpResponse(status=500, content="UserProfile doesn't exist")
 
+        # Return the details and nested profile
         return Response({
             'sessionid': request.session.session_key,
             'user': username,
@@ -102,7 +129,11 @@ class LoginViewSet(viewsets.ViewSet):
             'profile': profile_serialized.data
         })
 
-    def logout(self, request):
+    @classmethod
+    def logout(cls, request):
+        """Log out the user."""
+
+        # Delete the session key if we can
         try:
             del request.session['username']
         except KeyError:
