e73b1f373fa0a63006eaf156f40a85a164b91ecd
Varun Patil <radialapps@gmail.com>
2018-03-17 21:45:36 +0530

    Refactor data filling code

---
+++ backend/login/helpers.py
@@ -0,0 +1,33 @@
+"""Helpers for login functions."""
+
+def fill_models_from_sso(user_profile, user, profile_json):
+    """Fill models from SSO data."""
+
+    # Fill in data into the profile from SSO
+    for response_key, data_key in {
+            'first_name': 'name',
+            'email': 'email',
+            'mobile': 'contact_no',
+            'roll_number': 'roll_no'}.items():
+
+        if response_key in profile_json:
+            setattr(user_profile, data_key, profile_json[response_key])
+
+    # Fill in some special parameters
+    if 'first_name' in profile_json and 'last_name' in profile_json:
+        user_profile.name = profile_json['first_name'] + ' ' + profile_json['last_name']
+        user.first_name = profile_json['first_name']
+        user.last_name = profile_json['last_name']
+
+    if 'email' in profile_json:
+        user.email = profile_json['email']
+
+    if 'contacts' in profile_json and profile_json['contacts']:
+        user_profile.contact_no = profile_json['contacts'][0]['number']
+
+    if 'profile_picture' in profile_json:
+        user_profile.profile_pic = 'https://gymkhana.iitb.ac.in' + profile_json['profile_picture']
+
+    # Save the profile
+    user.save()
+    user_profile.save()
--- backend/login/views.py
+++ backend/login/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import login
 from django.contrib.auth import logout
 from rest_framework import viewsets
 from rest_framework.response import Response
+from login.helpers import fill_models_from_sso
 from users.models import UserProfile
 from users.serializers import UserProfileFullSerializer
 
@@ -85,34 +86,8 @@ class LoginViewSet(viewsets.ViewSet):
         except UserProfile.DoesNotExist:
             user_profile = UserProfile.objects.create(user=user, name='iitbuser')
 
-        # Fill in data into the profile from SSO
-        for response_key, data_key in {
-                'first_name': 'name',
-                'email': 'email',
-                'mobile': 'contact_no',
-                'roll_number': 'roll_no'}.items():
-
-            if response_key in profile_json:
-                setattr(user_profile, data_key, profile_json[response_key])
-
-        # Fill in some special parameters
-        if 'first_name' in profile_json and 'last_name' in profile_json:
-            user_profile.name = profile_json['first_name'] + ' ' + profile_json['last_name']
-            user.first_name = profile_json['first_name']
-            user.last_name = profile_json['last_name']
-
-        if 'email' in profile_json:
-            user.email = profile_json['email']
-
-        if 'contacts' in profile_json and profile_json['contacts']:
-            user_profile.contact_no = profile_json['contacts'][0]['number']
-
-        if 'profile_picture' in profile_json:
-            user_profile.profile_pic = 'https://gymkhana.iitb.ac.in' + profile_json['profile_picture']
-
-        # Save the profile
-        user.save()
-        user_profile.save()
+        # Fill models with new data
+        fill_models_from_sso(user_profile, user, profile_json)
 
         # Log in the user
         login(request, user)
