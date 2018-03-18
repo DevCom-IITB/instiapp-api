9bb0849ff1e760fd9191962eef99827a4de67ef7
Varun Patil <radialapps@gmail.com>
2018-03-19 01:29:16 +0530

    Create serializers and views for roles

--- backend/roles/views.py
+++ backend/roles/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from roles.models import BodyRole
+from roles.serializers import RoleSerializer
 
-# Create your views here.
+class BodyRoleViewSet(viewsets.ModelViewSet):   # pylint: disable=too-many-ancestors
+    """API endpoint that allows roles to be viewed or edited."""
+    queryset = BodyRole.objects.all()
+    serializer_class = RoleSerializer
