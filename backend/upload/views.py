81bedd39be81ac3c66f072a96ba38d71d8ed03c2
Varun Patil <radialapps@gmail.com>
2018-03-19 18:18:15 +0530

    Add authorization to upload image API (fixes #63)

--- backend/upload/views.py
+++ backend/upload/views.py
@@ -2,8 +2,19 @@
 from rest_framework import viewsets
 from upload.serializers import UploadedImageSerializer
 from upload.models import UploadedImage
+from roles.helpers import login_required_ajax
 
 class UploadViewSet(viewsets.ModelViewSet):   # pylint: disable=too-many-ancestors
     """API endpoint that allows files to be uploaded."""
     queryset = UploadedImage.objects.all()
     serializer_class = UploadedImageSerializer
+
+    @login_required_ajax
+    def create(self, request):
+        """Upload an image."""
+        return super().create(request)
+
+    @login_required_ajax
+    def destroy(self, request, pk):
+        """Delete an image entry."""
+        return super().destroy(request, pk)
