2db7717afa4eb37e87816d0660d3a38a545745fc
Varun Patil <radialapps@gmail.com>
2018-03-19 14:08:39 +0530

    Create an Institute role to create roles in any body (fixes #59)

--- backend/roles/views.py
+++ backend/roles/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from roles.models import BodyRole
 from roles.serializers import RoleSerializer
 from roles.helpers import user_has_privilege
+from roles.helpers import user_has_insti_privilege
 from roles.helpers import forbidden_no_privileges
 
 class BodyRoleViewSet(viewsets.ModelViewSet):   # pylint: disable=too-many-ancestors
@@ -13,6 +14,9 @@ class BodyRoleViewSet(viewsets.ModelViewSet):   # pylint: disable=too-many-ances
 
     def create(self, request):
         """Creates a body role if the user has privileges."""
+        if user_has_insti_privilege(request.user.profile, 'RoleB'):
+            return super().create(request)
+
         if not request.data['body']:
             return Response({"body": "body is required"}, status=400)
         if not user_has_privilege(request.user.profile, request.data['body'], 'Role'):
@@ -21,6 +25,9 @@ class BodyRoleViewSet(viewsets.ModelViewSet):   # pylint: disable=too-many-ances
 
     def update(self, request, pk):
         """Updates an existing body role if the user has privileges."""
+        if user_has_insti_privilege(request.user.profile, 'RoleB'):
+            return super().update(request, pk)
+
         body = BodyRole.objects.get(id=pk).body
         if request.data['body'] != str(body.id):
             return Response({'error': 'body is immutable'}, status=400)
@@ -30,6 +37,9 @@ class BodyRoleViewSet(viewsets.ModelViewSet):   # pylint: disable=too-many-ances
 
     def destroy(self, request, pk):
         """Deletes an existing body role if the user has privileges."""
+        if user_has_insti_privilege(request.user.profile, 'RoleB'):
+            return super().destroy(request, pk)
+
         bodyid = str(BodyRole.objects.get(id=pk).body.id)
         if not user_has_privilege(request.user.profile, bodyid, 'Role'):
             return forbidden_no_privileges()
