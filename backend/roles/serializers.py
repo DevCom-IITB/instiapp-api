9aa9b7780bf625aa77828741ab8b541893091b75
Varun Patil <radialapps@gmail.com>
2018-03-19 14:37:19 +0530

    Include details of body in roles Clients can figure out which bodies the user can create events from using this data (fixes #30)

--- backend/roles/serializers.py
+++ backend/roles/serializers.py
@@ -3,14 +3,16 @@ from roles.models import BodyRole
 from roles.models import InstituteRole
 from roles.models import PERMISSION_CHOICES
 from roles.models import INSTITUTE_PERMISSION_CHOICES
+from bodies.serializers import BodySerializerMin
 
 class RoleSerializer(serializers.ModelSerializer):
 
     permissions = serializers.MultipleChoiceField(choices=PERMISSION_CHOICES)
+    body_detail = BodySerializerMin(read_only=True, source='body')
 
     class Meta:
         model = BodyRole
-        fields = ('id', 'name', 'inheritable', 'body', 'permissions', 'users')
+        fields = ('id', 'name', 'inheritable', 'body', 'body_detail', 'permissions', 'users')
 
 class InstituteRoleSerializer(serializers.ModelSerializer):
 
