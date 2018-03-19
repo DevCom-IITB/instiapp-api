22b3be064fa38cfe8fcf166fda4b1ae0b57f65b3
Varun Patil <radialapps@gmail.com>
2018-03-19 12:18:45 +0530

    Authorize body update (fixes #43)

--- backend/bodies/views.py
+++ backend/bodies/views.py
@@ -5,6 +5,8 @@ from bodies.serializers_followers import BodyFollowersSerializer
 from bodies.serializers import BodySerializer
 from bodies.serializers import BodySerializerMin
 from bodies.models import Body
+from roles.helpers import user_has_privilege
+from roles.helpers import forbidden_no_privileges
 
 class BodyViewSet(viewsets.ModelViewSet):   # pylint: disable=too-many-ancestors
     """API endpoint that allows bodies to be viewed or edited."""
@@ -17,6 +19,12 @@ class BodyViewSet(viewsets.ModelViewSet):   # pylint: disable=too-many-ancestors
         queryset = Body.objects.all()
         serializer = BodySerializerMin(queryset, many=True)
         return Response(serializer.data)
+
+    def update(self, request, pk):
+        """Updates an existing body if the user has privileges."""
+        if not user_has_privilege(request.user.profile, pk, 'UpdB'):
+            return forbidden_no_privileges()
+        return super().update(request, pk)
 
 class BodyFollowersViewSet(viewsets.ModelViewSet):   # pylint: disable=too-many-ancestors
     """API endpoint that lists followers of bodies."""
