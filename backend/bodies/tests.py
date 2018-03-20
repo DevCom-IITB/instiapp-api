8c0b95f72a20025e560ada7fac483a0aba80b555
Varun Patil <radialapps@gmail.com>
2018-03-20 22:59:28 +0530

    Add body update unit test

--- backend/bodies/tests.py
+++ backend/bodies/tests.py
@@ -11,16 +11,18 @@ class BodyTestCase(APITestCase):
     test_body_1_id = None
     test_body_2_id = None
     user = None
+    insti_role = None
+    body_1_role = None
 
     def setUp(self):
         # Fake authenticate
         self.user = get_new_user()
         self.client.force_authenticate(self.user) # pylint: disable=E1101
 
-        insti_role = InstituteRole.objects.create(
+        self.insti_role = InstituteRole.objects.create(
             name='TestInstiRole',
             permissions=['AddB'])
-        self.user.profile.institute_roles.add(insti_role)
+        self.user.profile.institute_roles.add(self.insti_role)
 
         url = '/api/bodies'
         data = {
@@ -33,9 +35,9 @@ class BodyTestCase(APITestCase):
         self.test_body_1_id = response.data['id']
 
         test_body_1 = Body.objects.get(id=self.test_body_1_id)
-        body_1_role = BodyRole.objects.create(
+        self.body_1_role = BodyRole.objects.create(
             name="Body1Role", body=test_body_1, permissions='AddE')
-        self.user.profile.roles.add(body_1_role)
+        self.user.profile.roles.add(self.body_1_role)
 
         data = {
             'name': 'TestBody2',
@@ -56,12 +58,33 @@ class BodyTestCase(APITestCase):
         self.assertEqual(test_body.name, 'TestBody2')
         self.assertEqual(test_body.image_url, 'http://example.com/image2.png')
 
+    def test_body_update(self):
+        """Test if body can be updated with proper roles."""
+
+        url = '/api/bodies/' + self.test_body_1_id
+        data = {
+            'name': 'TestBody1Upd',
+            'image_url': 'http://example.com/image_new.png'
+        }
+
+        # Try without role
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, 403)
+
+        # Try with role
+        self.body_1_role.permissions += ',UpdB'
+        self.body_1_role.save()
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, 200)
+        test_body = Body.objects.get(id=self.test_body_1_id)
+        self.assertEqual(test_body.name, 'TestBody1Upd')
+
     def test_event_creation(self):
         """Test if events can be created for the body."""
 
         test_body_1 = Body.objects.get(id=self.test_body_1_id)
-        test_body_2 = Body.objects.get(id=self.test_body_2_id)
 
+        # Test simple body creation
         url = '/api/events'
         data = {
             "name": "TestEvent1",
@@ -77,3 +100,8 @@ class BodyTestCase(APITestCase):
         test_event = Event.objects.get(id=response.data['id'])
         self.assertEqual(test_event.name, 'TestEvent1')
         self.assertEqual(test_event.bodies.get(id=test_body_1.id), test_body_1)
+
+        # Check that bodies cannot be created without roles
+        data['bodies_id'] = [self.test_body_1_id, self.test_body_2_id]
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, 403)
