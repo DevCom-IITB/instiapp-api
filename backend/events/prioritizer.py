34e1b3fee60933f499df3528db3d2215df85761a
Varun Patil <radialapps@gmail.com>
2018-03-24 23:20:26 +0530

    Separate time dependent factor for followed bodies

--- backend/events/prioritizer.py
+++ backend/events/prioritizer.py
@@ -8,7 +8,8 @@ WEIGHT_START_TIME = 800                  # Weight of time from event start
 WEIGHT_END_TIME = 800                    # Weight of time from event end
 TIME_SD = 2.5                            # Standard deviation of time distribution
 TIME_L_END = 1.2                         # Lambda for exponential of ended penalty
-BODY_FOLLOWING_BONUS = 500               # Bonus if the body is followed
+BODY_FOLLOWING_BONUS = 300               # Bonus if the body is followed
+TIME_DEP_BODY_FOLLOWING_BONUS = 800      # Bonus if the body is followed dependent on time
 
 def get_prioritized(queryset, profile):
     now = timezone.now()
@@ -22,19 +23,20 @@ def get_prioritized(queryset, profile):
         start_time_diff = (event.start_time - now).total_seconds()  / 86400
 
         start_time_factor = math.exp((-(start_time_diff / TIME_SD)**2))
-        start_time_points = WEIGHT_START_TIME * start_time_factor
 
         # Apply exponential to and penalise finished events
         if event.end_time < now:
             event.weight -= FINISHED_PENALTY
             end_time_factor = math.exp(-abs(end_time_diff) / TIME_L_END)
-            start_time_points *= end_time_factor
+            start_time_factor *= end_time_factor
 
+        start_time_points = WEIGHT_START_TIME * start_time_factor
         event.weight += int(start_time_points)
 
         for body in event.bodies.all():
             if body in profile.followed_bodies.all():
-                event.weight += int(BODY_FOLLOWING_BONUS * start_time_factor)
+                event.weight += int(
+                    BODY_FOLLOWING_BONUS + (TIME_DEP_BODY_FOLLOWING_BONUS * start_time_factor))
 
         print(event.name, event.weight, event.start_time)
 
