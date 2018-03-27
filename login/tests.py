7f3ddd20bb37d95522792a8df4e4a2fc2549a422
Varun Patil <radialapps@gmail.com>
2018-03-28 00:58:56 +0530

    Add logout test

--- login/tests.py
+++ login/tests.py
@@ -18,16 +18,25 @@ class LoginTestCase(APITestCase):
     def test_login(self):
         """Test if SSO login works."""
 
+        # Start mock SSO server and wait to be sure it has started
         mock_server = Popen(['python', 'login/test_server.py'])
         time.sleep(1)
 
+        # Try to log in
         url = 'http://localhost/api/login?code=TEST_CODE&redir=REDIRECT_URI'
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data['profile']['name'], "First Name Last Name")
 
+        # Confirm we are logged in
         url = 'http://localhost/api/login/get-user'
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, 200)
 
+        # Terminate our server
         mock_server.terminate()
+
+        # Test logout
+        url = 'http://localhost/api/logout'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, 200)
