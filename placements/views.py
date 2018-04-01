ae757cb7d81e52e7c9c8e1a75ef600412a4255aa
Varun Patil <radialapps@gmail.com>
2018-04-02 00:23:36 +0530

    Make login required for placement blog

--- placements/views.py
+++ placements/views.py
@@ -4,9 +4,11 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 from placements.models import PlacementBlogEntry
 from placements.serializers import PlacementBlogEntrySerializer
+from roles.helpers import login_required_ajax
 
 class PlacementBlogViewset(viewsets.ViewSet):
 
+    @login_required_ajax
     def list(self, request):
         return Response(PlacementBlogEntrySerializer(PlacementBlogEntry.objects.filter(
             published__gte=timezone.now() - timedelta(days=15)), many=True).data)
