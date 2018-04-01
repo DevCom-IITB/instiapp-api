7392d2a6aa26acab6864b43d9fe1778b40e06f7e
Varun Patil <radialapps@gmail.com>
2018-04-01 13:48:41 +0530

    Modify some more admin pages

--- locations/admin.py
+++ locations/admin.py
@@ -3,5 +3,6 @@ from locations.models import Location
 
 class LocationAdmin(admin.ModelAdmin):
     list_filter = ('reusable',)
+    search_fields = ['name']
 
 admin.site.register(Location, LocationAdmin)
--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from users.models import UserProfile
 
-admin.site.register(UserProfile)
+class ProfileAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+admin.site.register(UserProfile, ProfileAdmin)
