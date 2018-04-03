4636f4b1f55474abf3f0ff94d164c2b9a144ba2e
Varun Patil <radialapps@gmail.com>
2018-04-04 00:25:22 +0530

    Return min serialized parents with bodies, refactor

--- bodies/serializers.py
+++ bodies/serializers.py
@@ -4,11 +4,6 @@ from events.prioritizer import get_r_fresh_prioritized_events
 from bodies.models import Body
 from bodies.serializer_min import BodySerializerMin
 
-class ChildrenSerializer(serializers.Serializer):   # pylint: disable=W0223
-    """Serializes children of the body from BodyChildRelation."""
-    def to_representation(self, instance):
-        return BodySerializerMin(instance.child, context=self.context).data
-
 class BodySerializer(serializers.ModelSerializer):
     """Serializer for Body."""
 
@@ -19,7 +14,7 @@ class BodySerializer(serializers.ModelSerializer):
         read_only=True)
 
     parents = serializers.SerializerMethodField()
-    children = ChildrenSerializer(many=True, read_only=True)
+    children = serializers.SerializerMethodField()
 
     events = serializers.SerializerMethodField()
     roles = RoleSerializerMin(many=True, read_only=True)
@@ -30,10 +25,15 @@ class BodySerializer(serializers.ModelSerializer):
                   'image_url', 'children', 'parents', 'events', 'followers_count',
                   'roles', 'website_url')
 
-    @classmethod
-    def get_parents(cls, obj):
-        """Gets a list of ids of parents of a Body."""
-        return [x.parent.id for x in obj.parents.all()]
+    @staticmethod
+    def get_parents(obj):
+        """Gets a min serialized parents of a Body."""
+        return [BodySerializerMin(x.parent).data for x in obj.parents.all()]
+
+    @staticmethod
+    def get_children(obj):
+        """Gets a min serialized children of a Body."""
+        return [BodySerializerMin(x.child).data for x in obj.children.all()]
 
     def get_events(self, obj):
         """Gets filtred events."""
