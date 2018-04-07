254220dabf8361e7e070ef71eaab64e92f447b02
Varun Patil <radialapps@gmail.com>
2018-04-07 13:51:42 +0530

    Expand login tests

--- login/tests.py
+++ login/tests.py
@@ -22,6 +22,22 @@ class LoginTestCase(APITestCase):
         mock_server = Popen(['python', 'login/test_server.py'])
         time.sleep(1)
 
+        # Check validation
+        # Without code
+        url = 'http://localhost/api/login?redir=REDIRECT_URI'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, 400)
+
+        # Without redir
+        url = 'http://localhost/api/login?code=TEST_CODE'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, 400)
+
+        # Try to log in with wrong code
+        url = 'http://localhost/api/login?code=T_CODE&redir=REDIRECT_URI'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, 400)
+
         # Try to log in
         url = 'http://localhost/api/login?code=TEST_CODE&redir=REDIRECT_URI&fcm_id=testfcm'
         response = self.client.get(url, format='json')
