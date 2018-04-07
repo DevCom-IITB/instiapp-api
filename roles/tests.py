6dc5b2c4c8921f9bd6290344e2553f6f8f89df7c
Varun Patil <radialapps@gmail.com>
2018-04-07 14:27:15 +0530

    Add preliminary tests for roles

--- roles/tests.py
+++ roles/tests.py
@@ -1,3 +1,27 @@
-# from django.test import TestCase
+"""Unit tests for Roles."""
+from rest_framework.test import APITestCase
+from bodies.models import Body
+from roles.models import BodyRole
+from roles.models import InstituteRole
+from login.tests import get_new_user
 
-# Create your tests here.
+class RoleTestCase(APITestCase):
+    """Tests for roles."""
+
+    body = None
+    bodyrole = None
+    instirole = None
+
+    def setUp(self):
+        # Fake authenticate
+        self.user = get_new_user()
+        self.client.force_authenticate(self.user) # pylint: disable=E1101
+
+        self.body = Body.objects.create(name="Body1")
+        self.bodyrole = BodyRole.objects.create(name="Role", body=self.body)
+        self.instirole = InstituteRole.objects.create(name="InstiRole")
+
+    def test_roles_other(self):
+        """Check misc parameters of Roles"""
+        self.assertEqual(str(self.instirole), self.instirole.name)
+        self.assertEqual(str(self.bodyrole), self.body.name + " " + self.bodyrole.name)
