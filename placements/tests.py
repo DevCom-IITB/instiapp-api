e014b23e4212eb86c59852f27bda29d7e18cc9c6
Varun Patil <radialapps@gmail.com>
2018-04-08 16:05:26 +0530

    Add training blog test

--- placements/tests.py
+++ placements/tests.py
@@ -8,12 +8,14 @@ class PlacementsTestCase(APITestCase):
     """Test placements endpoints."""
 
     entry1 = None
-    entry2 = None
 
     def setUp(self):
         # Create dummies
         self.entry1 = BlogEntry.objects.create(title="PEntry1", blog_url=settings.PLACEMENTS_URL)
-        self.entry2 = BlogEntry.objects.create(title="PEntry2", blog_url=settings.PLACEMENTS_URL)
+        BlogEntry.objects.create(title="PEntry2", blog_url=settings.PLACEMENTS_URL)
+        BlogEntry.objects.create(title="TEntry1", blog_url=settings.TRAINING_BLOG_URL)
+        BlogEntry.objects.create(title="TEntry2", blog_url=settings.TRAINING_BLOG_URL)
+        BlogEntry.objects.create(title="TEntry3", blog_url=settings.TRAINING_BLOG_URL)
 
     def test_placement_other(self):
         """Check misc parameters of Placement."""
@@ -33,3 +35,18 @@ class PlacementsTestCase(APITestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data), 2)
+
+    def test_training_get(self):
+        """Check auth before getting training blog."""
+
+        # Try without authentication
+        url = '/api/training-blog'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 401)
+
+        # Try after authentication
+        user = get_new_user()
+        self.client.force_authenticate(user) # pylint: disable=E1101
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 3)
