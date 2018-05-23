15ec437526bb4b3f019ec91249810f67d9d0e760
Varun Patil <radialapps@gmail.com>
2018-05-23 06:35:10 +0530

    Add test case for insufficient privilege SSO login

--- login/test_server.py
+++ login/test_server.py
@@ -22,6 +22,8 @@ class S(BaseHTTPRequestHandler):
         auth = str(self.headers["Authorization"])
         if "Bearer ACCESS_TOKEN" in auth:
             self.wfile.write(b'{"id":3,"username":"username","first_name":"First Name","type":"TYPE","profile_picture":"/sso/path/to/profile_picture_file","last_name":"Last Name","sex":"SEX","email":"username@iitb.ac.in","mobile":"0123456789","roll_number":"123456789","program":{"id":1,"department":"DEPARTMENT","department_name":"FULL_DEPARTMENT_NAME","join_year":2012,"graduation_year":2016,"degree":"DEGREE","degree_name":"FULL_DEGREE_NAME"},"secondary_emails":[{"id":1,"email":"user_email@gmail.com"}],"contacts":[{"id":1,"number":"9876543210"}],"insti_address":{"id":1,"room":"room_number","hostel":"HOSTEL","hostel_name":"FULL_HOSTEL_NAME"}}')
+        elif "Bearer LOW_PRIV_ACCESS_TOKEN" in auth:
+            self.wfile.write(b'{"id":3,"username":"username"}')
         else:
             self.wfile.write(b'{"error":"wrong access token"}')
 
@@ -38,6 +40,7 @@ class S(BaseHTTPRequestHandler):
         token_url = "CODE_TOKEN"
         bad_token_test = "code=BAD_TEST_CODE"
         token_test = "code=TEST_CODE&redirect_uri=REDIRECT_URI&grant_type=authorization_code"
+        token_test_lp = "code=TEST_CODE_LP&redirect_uri=REDIRECT_URI&grant_type=authorization_code"
 
         if token_url in self.path and bad_token_test in str(post_data):
             self.wfile.write(b'{"access_token":"BAD_ACCESS_TOKEN"}')
@@ -45,6 +48,8 @@ class S(BaseHTTPRequestHandler):
 
         if token_url in self.path and token_test in str(post_data):
             self.wfile.write(b'{"access_token":"ACCESS_TOKEN"}')
+        elif token_url in self.path and token_test_lp in str(post_data):
+            self.wfile.write(b'{"access_token":"LOW_PRIV_ACCESS_TOKEN"}')
         else:
             self.wfile.write(b'{"error":"auth test failed"}')
 
