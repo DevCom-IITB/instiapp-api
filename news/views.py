b0bd2d8990b9e9efc84b0e67835bfbb607e7e6a3
Varun Patil <radialapps@gmail.com>
2018-05-25 22:07:56 +0530

    Add news API endpoint

--- news/views.py
+++ news/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import viewsets
+from news.models import NewsEntry
+from news.serializers import NewsEntrySerializer
 
-# Create your views here.
+class NewsFeedViewset(viewsets.ViewSet):
+
+    @staticmethod
+    def news_feed(request):
+        """Get Feed."""
+        return Response(NewsEntrySerializer(NewsEntry.objects.all()[0:30], many=True).data)
