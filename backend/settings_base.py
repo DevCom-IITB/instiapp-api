77bc3d4bc10cdffbdd4c515bd890705d8206752c
Varun Patil <radialapps@gmail.com>
2018-05-25 21:42:10 +0530

    Disable LDAP username-password warning

--- backend/settings_base.py
+++ backend/settings_base.py
@@ -131,5 +131,4 @@ LDAP_PASSWORD = None
 if 'LDAP_USERNAME' in os.environ and 'LDAP_PASSWORD' in os.environ:
     LDAP_USERNAME = os.environ['LDAP_USERNAME']
     LDAP_PASSWORD = os.environ['LDAP_PASSWORD']
-else:
-    print('WARN: LDAP username and password not present in environment.')
+    print('INFO: LDAP username and password present in environment.')
