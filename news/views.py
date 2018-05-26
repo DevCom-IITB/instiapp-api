44f858e26209571008d7ab965242d13b8e23d87a
Varun Patil <radialapps@gmail.com>
2018-05-26 10:17:57 +0530

    Add paging with query parameter to news (fix #123)

--- news/views.py
+++ news/views.py
@@ -8,4 +8,17 @@ class NewsFeedViewset(viewsets.ViewSet):
     @staticmethod
     def news_feed(request):
         """Get Feed."""
-        return Response(NewsEntrySerializer(NewsEntry.objects.all()[0:30], many=True).data)
+        # Initialize defaults
+        from_i = 0
+        num = 30
+
+        # Set values from query paramters if available and valid
+        from_q = request.GET.get('from')
+        num_q = request.GET.get('num')
+        if from_q is not None and str.isdigit(from_q):
+            from_i = int(from_q)
+        if num_q is not None and str.isdigit(num_q) and int(num_q) <= 100:
+            num = int(num_q)
+
+        return Response(NewsEntrySerializer(
+            NewsEntry.objects.all()[from_i : from_i + num], many=True).data)
