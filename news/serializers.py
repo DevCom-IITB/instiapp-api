2dc314e70e1f63dc030bac80614ebd1d29b261ba
Varun Patil <radialapps@gmail.com>
2018-06-01 14:20:14 +0530

    Change get_reactions_count to staticmethod

--- news/serializers.py
+++ news/serializers.py
@@ -9,7 +9,8 @@ class NewsEntrySerializer(serializers.ModelSerializer):
 
     reactions_count = serializers.SerializerMethodField()
 
-    def get_reactions_count(self, obj):
+    @staticmethod
+    def get_reactions_count(obj):
         """Get number of user reactions on news item."""
         return {t : obj.reacted_by.filter(unr__reaction=t).count() for t in range(0, 6)}
 
