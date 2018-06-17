5a33039a82d29c2f045d75226b2315d8828475b8
Varun Patil <radialapps@gmail.com>
2018-06-17 15:33:32 +0530

    Add search endpoint

---
+++ other/tests.py
@@ -0,0 +1,48 @@
+"""Unit tests for news feed."""
+from rest_framework.test import APITestCase
+from django.utils import timezone
+
+from bodies.models import Body
+from events.models import Event
+from users.models import UserProfile
+
+class OtherTestCase(APITestCase):
+    """Test other endpoints."""
+
+    def setUp(self):
+        # Create bodies
+        Body.objects.create(name="Test Body1")
+        Body.objects.create(name="Test Body2")
+
+        Event.objects.create(name="Test Event1", start_time=timezone.now(), end_time=timezone.now())
+        Event.objects.create(name="Test Event2 Body1", start_time=timezone.now(), end_time=timezone.now())
+        Event.objects.create(name="Test Event21", start_time=timezone.now(), end_time=timezone.now())
+
+        UserProfile.objects.create(name="Test User1")
+        UserProfile.objects.create(name="Test User2")
+
+
+    def test_search(self):
+        """Test the search endpoint."""
+        url = '/api/search?query='
+
+        response = self.client.get(url + 'bod')
+        self.assertEqual(response.status_code, 400)
+
+        response = self.client.get(url + 'body1')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['bodies']), 1)
+        self.assertEqual(len(response.data['events']), 1)
+        self.assertEqual(len(response.data['users']), 0)
+
+        response = self.client.get(url + 'body2')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['bodies']), 1)
+        self.assertEqual(len(response.data['events']), 0)
+        self.assertEqual(len(response.data['users']), 0)
+
+        response = self.client.get(url + 'test user')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['bodies']), 0)
+        self.assertEqual(len(response.data['events']), 0)
+        self.assertEqual(len(response.data['users']), 2)
---
+++ other/views.py
@@ -0,0 +1,39 @@
+"""Views that don't fit anywhere else."""
+from django.db.models import Q
+from rest_framework.response import Response
+from rest_framework import viewsets
+
+from bodies.models import Body
+from bodies.serializer_min import BodySerializerMin
+from events.models import Event
+from events.serializers import EventSerializer
+from users.models import UserProfile
+from users.serializers import UserProfileSerializer
+
+class OtherViewset(viewsets.ViewSet):
+
+    @staticmethod
+    def search(request):
+        """EXPENSIVE: Search with query param `query` throughout the database."""
+
+        req_query = request.GET.get("query")
+        if not req_query or len(req_query) < 5:
+            return Response({"message": "No query or too short!"}, status=400)
+
+        # Search bodies by name and description
+        bodies = Body.objects.filter(
+            Q(name__icontains=req_query) | Q(description__icontains=req_query))
+
+        # Search events by name and description
+        events = Event.objects.filter(
+            Q(name__icontains=req_query) | Q(description__icontains=req_query))
+
+        # Search users by only name: don't add anything else here
+        users = UserProfile.objects.filter(
+            Q(name__icontains=req_query))
+
+        return Response({
+            "bodies": BodySerializerMin(bodies, many=True).data,
+            "events": EventSerializer(events, many=True).data,
+            "users": UserProfileSerializer(users, many=True).data
+        })
