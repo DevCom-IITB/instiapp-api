55c7653f076c4aef0ae62158166ecac041fd26ee
HrushikeshBodas <31095519+HrushikeshBodas@users.noreply.github.com>
2018-06-17 14:57:07 +0530

    Update serializers.py

--- messmenu/serializers.py
+++ messmenu/serializers.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers
 from messmenu.models import MenuDetails
 from messmenu.models import Hostel
+
+class MenuDetailsSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = MenuDetails
+		fields = '__all__'
+		
 class HostelSerializer(serializers.ModelSerializer):
 	"""Serializer for the hostel model"""
 	WeeklyMenu = MenuDetailsSerializer(many=True)
 	class Meta:
 		model = Hostel
 		fields = ('hostel')
-class MenuDetailsSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = MenuDetails
-		fields = '__all__'
