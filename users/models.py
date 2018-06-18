af3edadb65d4778f72ae307e15169eaa9dc04990
Varun Patil <radialapps@gmail.com>
2018-06-19 03:06:40 +0530

    Remove commented code from user model

--- users/models.py
+++ users/models.py
@@ -42,12 +42,3 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.name
-
-    # @receiver(post_save, sender=User)
-    # def create_user_profile(sender, instance, created, **kwargs):
-    #     if created:
-    #         p = Profile.objects.create(user=instance)
-
-    # @receiver(post_save, sender=User)
-    # def save_user_profile(sender, instance, **kwargs):
-    #     instance.profile.save()
