de4a20ecaeed4ca2e5452b3bdbfd1b011d712989
Varun Patil <radialapps@gmail.com>
2018-06-25 01:05:11 +0530

    Add map fields to locations model

--- locations/models.py
+++ locations/models.py
@@ -12,7 +12,16 @@ class Location(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     time_of_creation = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=50)
+
+    name = models.CharField(max_length=150)
+    short_name = models.CharField(max_length=50, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    parent = models.ForeignKey('self', blank=True, null=True, on_delete=models.SET_NULL)
+    parent_relation = models.CharField(max_length=20, blank=True, null=True)
+    group_id = models.IntegerField(blank=True, null=True)
+
+    pixel_x = models.IntegerField(blank=True, null=True)
+    pixel_y = models.IntegerField(blank=True, null=True)
     lat = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
     lng = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
     reusable = models.BooleanField(default=False)
