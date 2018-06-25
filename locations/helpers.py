209854e5adba202f9ea4222d794af07579ea0c2d
Varun Patil <radialapps@gmail.com>
2018-06-25 12:13:59 +0530

    Allow reusing unreusable locations silently

--- locations/helpers.py
+++ locations/helpers.py
@@ -4,16 +4,16 @@ from locations.models import Location
 
 def create_unreusable_locations(loc_list):
     """Takes a list of venues and creates venues which do not match
-    any existing reusable venues. Returns a list of venue ids to be
+    any existing venues. Returns a list of venue ids to be
     stored by the consumer."""
 
     # List of location ids to be returned
     loc_ids = []
 
-    # Reuse reusable venues, add new otherwise
+    # Reuse venues, add new otherwise
     for loc_name in loc_list:
         locs = Location.objects.filter(Q(name=loc_name) | Q(short_name=loc_name))
-        if locs.count() == 0 or not locs[0].reusable:
+        if not locs.exists():
             new_loc = Location.objects.create(name=loc_name, short_name=loc_name)
             new_loc.save()
             loc_ids.append(str(new_loc.id))
