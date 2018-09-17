64015308fcf4329cbb5530e824be385e2779deac
sshivam95 <sshivam95@gmail.com>
2018-09-17 18:20:34 +0530

    venter: update tests for delete and update of comment

--- venter/views.py
+++ venter/views.py
@@ -1,5 +1,3 @@
-from idlelib.FormatParagraph import get_comment_header
-from uuid import UUID
 from rest_framework.generics import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.response import Response
