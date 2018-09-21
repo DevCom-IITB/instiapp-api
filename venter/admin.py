e10a04a05faef6aff2566cd03fd96452caf7eaff
Varun Patil <radialapps@gmail.com>
2018-09-21 16:30:19 +0530

    Fix up whitespace and other conventions in venter

--- venter/admin.py
+++ venter/admin.py
@@ -1,51 +1,45 @@
 from django.contrib import admin
-
 from venter.models import Complaints
 from venter.models import Comment
 from venter.models import TagUris
 from venter.models import ComplaintMedia
 
-
 class CommentTabularInline(admin.TabularInline):
     model = Comment
     readonly_fields = ('text', 'time', 'commented_by',)
 
-
 class TagTabularInline(admin.TabularInline):
     model = Complaints.tags.through
-    verbose_name = "Tag"
-    verbose_name_plural = "Tags"
-
+    verbose_name = 'Tag'
+    verbose_name_plural = 'Tags'
 
 class UserLikedTabularInline(admin.TabularInline):
     model = Complaints.users_up_voted.through
-    verbose_name = "User up Voted"
-    verbose_name_plural = "Users up voted"
+    verbose_name = 'User up Voted'
+    verbose_name_plural = 'Users up voted'
 
 class ComplaintMediaTabularInline(admin.TabularInline):
     model = ComplaintMedia
     readonly_fields = ('image_url',)
 
 class ComplaintMediaModelAdmin(admin.ModelAdmin):
-    list_display = ["image_url", "complaint"]
+    list_display = ['image_url', 'complaint']
     model = ComplaintMedia
 
 class CommentModelAdmin(admin.ModelAdmin):
-    list_display = ["__str__", "commented_by", "complaint", "time"]
+    list_display = ['__str__', 'commented_by', 'complaint', 'time']
     model = Comment
 
-
 class ComplaintModelAdmin(admin.ModelAdmin):
-    list_display = ["__str__", "created_by", "report_date", "status"]
-    list_editable = ["status"]
-    list_filter = ["status"]
+    list_display = ['__str__', 'created_by', 'report_date', 'status']
+    list_editable = ['status']
+    list_filter = ['status']
     inlines = [CommentTabularInline, TagTabularInline, UserLikedTabularInline, ComplaintMediaTabularInline]
     exclude = ('tags', 'users_up_voted', 'media',)
-    search_fields = ["status", "description", "created_by__name"]
+    search_fields = ['status', 'description', 'created_by__name']
 
     class Meta:
         model = Complaints
-
 
 admin.site.register(Complaints, ComplaintModelAdmin)
 admin.site.register(Comment, CommentModelAdmin)
