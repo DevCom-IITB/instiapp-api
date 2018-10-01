3b3efa3550990da07aeae5e637ae1e066723367f
Varun Patil <radialapps@gmail.com>
2018-10-01 23:05:37 +0530

    Make venter urls consistent with new styling

--- venter/urls.py
+++ venter/urls.py
@@ -4,19 +4,19 @@ from venter.views import ComplaintViewSet
 from venter.views import CommentViewSet
 
 urlpatterns = [
-    path('complaints', ComplaintViewSet.as_view(
-        {'post': 'create','get':'list'}
-    )),
+    path('complaints', ComplaintViewSet.as_view({
+        'post': 'create', 'get': 'list'
+    })),
 
-    path('complaints/<pk>', ComplaintViewSet.as_view(
-        {'get': 'retrieve', 'put': 'update'}
-    )),
+    path('complaints/<pk>', ComplaintViewSet.as_view({
+        'get': 'retrieve', 'put': 'update'
+    })),
 
-    path('complaints/<pk>/comments', CommentViewSet.as_view(
-        {'post': 'create'}
-    )),
+    path('complaints/<pk>/comments', CommentViewSet.as_view({
+        'post': 'create'
+    })),
 
-    path('comments/<pk>', CommentViewSet.as_view(
-        {'put': 'update', 'delete': 'destroy', 'get': 'retrieve'}
-    )),
+    path('comments/<pk>', CommentViewSet.as_view({
+        'put': 'update', 'delete': 'destroy', 'get': 'retrieve'
+    })),
 ]
