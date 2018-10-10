92d5a68578c8a887a9b616d409f6ae83c6cbb0fb
Varun Patil <radialapps@gmail.com>
2018-10-10 16:47:32 +0530

    Refactor push-notify chore into smaller functions

--- other/management/commands/push-notify.py
+++ other/management/commands/push-notify.py
@@ -22,10 +22,128 @@ def send_push(subscription, payload):
         }
     )
 
+def send_notification_webpush(subscription, data_message):
+    """Attempt to send a data_message to a subscription."""
+    # Get a dict in the format we want
+    dict_sub = {
+        "endpoint": subscription.endpoint,
+        "keys": {
+            "p256dh": subscription.p256dh,
+            "auth": subscription.auth
+        }
+    }
+
+    # Make the payload
+    payload = {
+        "notification": {
+            "title": data_message['title'],
+            "body": data_message['verb'],
+            "icon": "assets/logo-sq-sm.png",
+            "vibrate": [100, 50, 100],
+            "data": data_message
+        }
+    }
+
+    try:
+        # Try to send
+        send_push(dict_sub, payload)
+        return 1
+    except WebPushException:
+        # Remove subscription
+        subscription.delete()
+
+    return 0
+
+def send_notification_fcm(push_service, profile, data_message):
+    """Attempt to send a single FCM notification."""
+
+    try:
+        registration_id = profile.fcm_id
+
+        # Check if the user supports rich notifications
+        if profile.android_version >= 17:
+            push_service.notify_single_device(
+                registration_id=registration_id, data_message=data_message)
+        else:
+            push_service.notify_single_device(
+                registration_id=registration_id, message_title=data_message['title'],
+                message_body=data_message['verb'], data_message=data_message, sound='default')
+
+        return 1
+
+    except Exception as ex:  # pylint: disable=W0703
+        print(profile.name, ex)
+
+    return 0
+
 def get_news_image(news):
     if 'yt:video' in news.guid:
         return 'https://img.youtube.com/vi/' + news.guid.split('video:')[1] + '/maxresdefault.jpg'
     return None
+
+def get_rich_notification(notification):
+    # Get title
+    title = "InstiApp"
+
+    # Default values
+    notification_type = None
+    notification_id = None
+    notification_extra = None
+
+    # Rich fields
+    notification_large_icon = None
+    notification_large_content = None
+    notification_image = None
+
+    # Get information about actor
+    actor = notification.actor
+    if actor is not None:
+        # Infer notification type from actor class
+        notification_type = actor.__class__.__name__.lower()
+
+        # Event
+        if isinstance(actor, Event):
+            title = actor.name
+            # Rich notifications
+            bodies = actor.bodies.all()
+            if bodies.exists():
+                notification_large_icon = bodies.first().image_url
+
+        # News/Blog Entry
+        if isinstance(actor, (BlogEntry, NewsEntry)):
+            title = actor.title
+            notification_extra = actor.link
+
+        # Rich field for news entry
+        if isinstance(actor, NewsEntry):
+            notification_large_icon = actor.body.image_url
+            notification_large_content = BeautifulSoup(actor.content, features='html5lib').text
+            if len(notification_large_content) > 250:
+                notification_large_content = notification_large_content[:250] + ' ...'
+            notification_image = get_news_image(actor)
+
+        notification_id = str(actor.id)
+
+    # Construct the data message
+    data_message = {
+        "type": notification_type,
+        "id": notification_id,
+        "extra": notification_extra,
+        "notification_id": str(notification.id),
+        "title": title,
+        "verb": notification.verb
+    }
+
+    # Set rich fields if present
+    if notification_large_icon is not None:
+        data_message['large_icon'] = notification_large_icon
+    if notification_large_content is not None:
+        data_message['large_content'] = notification_large_content
+    if notification_image is not None:
+        data_message['image_url'] = notification_image
+
+    return data_message
+
 
 class Command(BaseCommand):
     help = 'Sends push notifications'
@@ -33,10 +151,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):  # noqa: C901
         """Send Push notifications."""
 
-        sent = 0
-        failed = 0
-        fcm = 0
-        badfcm = 0
+        webpush_sent = 0
+        webpush_total = 0
+        fcm_sent = 0
+
         push_service = FCMNotification(api_key=settings.FCM_SERVER_KEY)
 
         # Iterate all unsent notifications
@@ -58,121 +176,20 @@ class Command(BaseCommand):
             notification.emailed = True
             notification.save()
 
-            # Get title
-            title = "InstiApp"
-
-            # Default values
-            notification_type = None
-            notification_id = None
-            notification_extra = None
-
-            # Rich fields
-            is_rich = False
-            notification_large_icon = None
-            notification_large_content = None
-            notification_image = None
-
-            # Get information about actor
-            actor = notification.actor
-            if actor is not None:
-                # Infer notification type from actor class
-                notification_type = actor.__class__.__name__.lower()
-
-                # Event
-                if isinstance(actor, Event):
-                    title = actor.name
-                    # Rich notifications
-                    bodies = actor.bodies.all()
-                    if bodies.exists():
-                        notification_large_icon = bodies.first().image_url
-
-                # News/Blog Entry
-                if isinstance(actor, (BlogEntry, NewsEntry)):
-                    title = actor.title
-                    notification_extra = actor.link
-
-                # Rich field for news entry
-                if isinstance(actor, NewsEntry):
-                    notification_large_icon = actor.body.image_url
-                    notification_large_content = BeautifulSoup(actor.content, features='html5lib').text
-                    if len(notification_large_content) > 250:
-                        notification_large_content = notification_large_content[:250] + ' ...'
-                    notification_image = get_news_image(actor)
-
-                notification_id = str(actor.id)
-
-            # Construct the data message
-            data_message = {
-                "type": notification_type,
-                "id": notification_id,
-                "extra": notification_extra,
-                "notification_id": str(notification.id),
-                "title": title,
-                "verb": notification.verb
-            }
-
-            # Set rich fields if present
-            if notification_large_icon is not None:
-                is_rich = True
-                data_message['large_icon'] = notification_large_icon
-            if notification_large_content is not None:
-                is_rich = True
-                data_message['large_content'] = notification_large_content
-            if notification_image is not None:
-                is_rich = True
-                data_message['image_url'] = notification_image
+            # Get rich notification
+            data_message = get_rich_notification(notification)
 
             # Send FCM push notification
-            try:
-                registration_id = profile.fcm_id
-                message_title = title
-                message_body = notification.verb
-
-                # Check if the user supports rich notifications
-                if is_rich and profile.android_version >= 17:
-                    push_service.notify_single_device(
-                        registration_id=registration_id, data_message=data_message)
-                else:
-                    push_service.notify_single_device(
-                        registration_id=registration_id, message_title=message_title,
-                        message_body=message_body, data_message=data_message, sound='default')
-
-                fcm += 1
-            except Exception as ex:  # pylint: disable=W0703
-                print(profile.name, ex)
-                badfcm += 1
+            fcm_sent += send_notification_fcm(push_service, profile, data_message)
 
             # For each web push subscription
             for subscription in profile.web_push_subscriptions.all():
+                webpush_total += 1
+                webpush_sent += send_notification_webpush(subscription, data_message)
 
-                # Get a dict in the format we want
-                dict_sub = {
-                    "endpoint": subscription.endpoint,
-                    "keys": {
-                        "p256dh": subscription.p256dh,
-                        "auth": subscription.auth
-                    }
-                }
-
-                # Make the payload
-                payload = {
-                    "notification": {
-                        "title": title,
-                        "body": notification.verb,
-                        "icon": "assets/logo-sq-sm.png",
-                        "vibrate": [100, 50, 100],
-                        "data": data_message
-                    }
-                }
-
-                try:
-                    # Try to send
-                    send_push(dict_sub, payload)
-                    sent += 1
-                except WebPushException:
-                    # Remove subscription
-                    failed += 1
-                    subscription.delete()
-
-        print("WebPush:", sent, "WebPush FAIL:", failed, "FCM", fcm, "FCM FAIL:", badfcm)
+        print(
+            "WebPush:", webpush_sent,
+            "WebPush FAIL:", webpush_total - webpush_sent,
+            "FCM:", fcm_sent
+        )
         self.stdout.write(self.style.SUCCESS('Push notification chore completed successfully'))
