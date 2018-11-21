6041a79463b4675ef4e244cce68810118a257400
unknown <ajoshi1996@gmail.com>
2018-11-21 16:39:38 +0530

    Removed notifications code, tests and modified venter code to match standards

--- venter/admin.py
+++ venter/admin.py
@@ -42,7 +42,7 @@ class AuthoritiesModelAdmin(admin.ModelAdmin):
 
 class ComplaintModelAdmin(admin.ModelAdmin):
     readonly_fields = ['created_by']
-    list_display = ['report_date', 'status', 'authority_email', 'created_by']
+    list_display = ['report_date', 'status', 'authority_email']
     list_editable = ['status', 'authority_email']
     list_filter = ['status']
     inlines = [CommentTabularInline, TagTabularInline, UserLikedTabularInline, ComplaintMediaTabularInline]
