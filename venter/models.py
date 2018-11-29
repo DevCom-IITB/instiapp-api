c4575a5d2c26022ec14e9e414791d3c3dc9ffe78
unknown <ajoshi1996@gmail.com>
2018-11-29 12:49:43 +0530

    revert to master

--- venter/models.py
+++ venter/models.py
@@ -46,12 +46,7 @@ class Complaints(models.Model):
     location_description = models.TextField(blank=True, null=True)
     tags = models.ManyToManyField(TagUris, related_name='tags', blank=True)
     users_up_voted = models.ManyToManyField('users.UserProfile', related_name='users_up_voted', blank=True)
-    authorities = models.ManyToManyField(Authorities, related_name='complaints')
-    email_status = models.BooleanField(default=False)
-
-    def email_list(self):
-        return list(self.authorities.all().values_list('name', flat=True))
-    email_list.short_description = 'List of Authorities'
+    authority_email = models.ForeignKey(Authorities, on_delete=models.SET_NULL, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Complaint'
