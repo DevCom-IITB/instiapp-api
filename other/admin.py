6169277737706e84c03c88814fac6bd533faaf04
Varun Patil <radialapps@gmail.com>
2019-01-23 18:43:28 +0530

    feat(fcm): Make device admin pretty

--- other/admin.py
+++ other/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from other.models import Device
 
-admin.site.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_filter = ('application', 'last_ping', 'platform', 'app_version')
+    list_display = ('user', 'application', 'platform', 'last_ping', 'app_version')
+    search_fields = ['user__name']
+
+
+admin.site.register(Device, DeviceAdmin)
