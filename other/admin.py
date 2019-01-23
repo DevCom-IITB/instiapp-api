b97b8fbeccdc9e9485455481e84b3ca1bdc6fd83
Varun Patil <radialapps@gmail.com>
2019-01-23 19:14:32 +0530

    fix(fcm): Order device list by last_ping desc in admin

--- other/admin.py
+++ other/admin.py
@@ -4,6 +4,7 @@ from other.models import Device
 class DeviceAdmin(admin.ModelAdmin):
     list_filter = ('application', 'last_ping', 'platform', 'app_version')
     list_display = ('user', 'application', 'platform', 'last_ping', 'app_version')
+    ordering = ('-last_ping',)
     search_fields = ['user__name']
 
 
