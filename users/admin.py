fbdcd57821ee6895b225a1752f0b975d4e8f7b48
Varun Patil <radialapps@gmail.com>
2019-01-31 14:54:03 +0530

    feat(admin): Add columns and filters to UserProfile

--- users/admin.py
+++ users/admin.py
@@ -5,7 +5,9 @@ from users.models import UserTagCategory
 from users.models import UserTag
 
 class ProfileAdmin(admin.ModelAdmin):
-    search_fields = ['name']
+    search_fields = ['name', 'roll_no']
+    list_display = ('name', 'roll_no', 'department', 'degree')
+    list_filter = ('join_year', 'department', 'degree')
 
 
 admin.site.register(UserProfile, ProfileAdmin)
