ce3e5e0450c1f2f1879845d1e81b35f92f3f2e31
Varun Patil <radialapps@gmail.com>
2019-01-31 15:34:24 +0530

    refactor(upload): Split upload images test

--- upload/tests.py
+++ upload/tests.py
@@ -14,13 +14,23 @@ from helpers.test_helpers import create_body
 
 from upload.models import UploadedImage
 
+get_image = lambda: SimpleUploadedFile(
+    "img.jpg", open("./upload/img.jpg", "rb").read(), content_type="image/jpeg")
+
+new_upload = lambda slf: slf.client.post('/api/upload', {'picture': get_image()})
+
 class UploadTestCase(APITestCase):
     """Check if logged in users can upload files."""
 
-    def test_upload(self):  # pylint: disable=R0914, R0915
+    def setUp(self):
+        self.user = get_new_user()
+        self.client.force_login(self.user)
+
+    def test_upload(self):
         """Test if logged in users can upload files."""
 
         # Try without authentication
+        self.client.logout()
         url = '/api/upload'
         data = {
             "picture": open('./upload/b64img.txt', 'r').read()
@@ -28,10 +38,8 @@ class UploadTestCase(APITestCase):
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, 401)
 
-        # Fake authenticate and try again
-        user = get_new_user()
-        self.client.force_authenticate(user)  # pylint: disable=E1101
-
+        # Try again with login
+        self.client.force_login(self.user)
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, 201)
 
@@ -49,25 +57,23 @@ class UploadTestCase(APITestCase):
         self.assertEqual(response.status_code, 204)
 
         # Check POST file upload
-        url = '/api/upload'
-        image = lambda: SimpleUploadedFile(
-            "img.jpg", open("./upload/img.jpg", "rb").read(), content_type="image/jpeg")
-        new_upload = lambda: self.client.post(url, {'picture': image()})
-        response = new_upload()
+        response = new_upload(self)
 
         # Check if the file was uploaded
         img_id = response.data['id']
         img = UploadedImage.objects.get(pk=img_id)
         self.assertIn('.jpg', str(img.picture))
 
-        # Check clean images chore
+    def test_clean_images_chore(self):
+        """Check clean images chore."""
+
         res = [None] * 5
-        res[0] = new_upload()
+        res[0] = new_upload(self)
 
         # Create images more than an hour ago
         with freeze_time(timezone.now() - timedelta(hours=3)):
             for i in range(1, 5):
-                res[i] = new_upload()
+                res[i] = new_upload(self)
 
             event1 = create_event(image_url=res[2].data['picture'])
             body1 = create_body(image_url=res[3].data['picture'])
