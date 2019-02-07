e34df0d8b96754613d70faa6d1a435fdd3970349
Varun Patil <radialapps@gmail.com>
2019-02-07 14:12:37 +0530

    perf(admin): Change user to raw_id_field everywhere (fix #355) (#357)

--- events/admin.py
+++ events/admin.py
@@ -6,11 +6,13 @@ class EventAdmin(admin.ModelAdmin):
     list_display = ('name', 'start_time', 'end_time')
     search_fields = ['name']
     ordering = ('-start_time',)
+    raw_id_fields = ('created_by',)
 
 class UESAdmin(admin.ModelAdmin):
     list_filter = ('event__start_time',)
     list_display = ('event', 'user', 'status')
     ordering = ('-event__start_time',)
+    raw_id_fields = ('user', 'event')
 
 
 admin.site.register(Event, EventAdmin)
--- upload/admin.py
+++ upload/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from upload.models import UploadedImage
 
-admin.site.register(UploadedImage)
+class UploadedImageAdmin(admin.ModelAdmin):
+    list_display = ('uploaded_by', 'is_claimed', 'claimant')
+    raw_id_fields = ('uploaded_by',)
+
+
+admin.site.register(UploadedImage, UploadedImageAdmin)
