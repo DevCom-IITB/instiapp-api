895a604dee79bfaa76c3142a111bb950ae00e22d
Varun Patil <radialapps@gmail.com>
2019-02-11 16:35:20 +0530

    Remove FaultTolerantTask

--- other/tasks.py
+++ other/tasks.py
@@ -3,9 +3,8 @@ from celery import shared_task
 from django.contrib.auth.models import User
 from notifications.signals import notify
 from events.models import Event
-from helpers.celery_fault import FaultTolerantTask
 
-@shared_task(base=FaultTolerantTask)
+@shared_task()
 def notify_new_event(pk):
     """Notify users about event creation."""
     instance = Event.objects.filter(id=pk).first()
@@ -20,7 +19,7 @@ def notify_new_event(pk):
             verb=body.name + " has added a new event"
         )
 
-@shared_task(base=FaultTolerantTask)
+@shared_task()
 def notify_upd_event(pk):
     """Notify users about event updation."""
     instance = Event.objects.filter(id=pk).first()
