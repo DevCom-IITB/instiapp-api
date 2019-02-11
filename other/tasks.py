94ae280ea8ac632e2e1bf1990b72fd5c59fb1055
Varun Patil <radialapps@gmail.com>
2019-02-11 17:50:07 +0530

    Add tasks to omit coveragerc

--- other/tasks.py
+++ other/tasks.py
@@ -5,8 +5,6 @@ from notifications.signals import notify
 from events.models import Event
 from helpers.celery_fault import FaultTolerantTask
 
-# pragma: no cover
-
 @shared_task(base=FaultTolerantTask)
 def notify_new_event(pk):
     """Notify users about event creation."""
