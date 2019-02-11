71fe81ed63a86880b00c000b906a0315f882a828
Varun Patil <radialapps@gmail.com>
2019-02-11 16:39:33 +0530

    Disable lint warning in FaultTolerantTask

--- helpers/celery_fault.py
+++ helpers/celery_fault.py
@@ -1,6 +1,8 @@
 from django.db import connection
 import celery
 
+# pylint: disable=W0223
+
 class FaultTolerantTask(celery.Task):
     """ Implements after return hook to close the invalid connection.
     This way, django is forced to serve a new connection for the next
