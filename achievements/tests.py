6d99a13fdef1f627dfb673b9ab5c3c86e7a2c1b9
Varun Patil <varun@mercari.com>
2019-07-19 21:05:40 +0900

    test(achievements): Add test for retrieve

--- achievements/tests.py
+++ achievements/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
+from login.tests import get_new_user
+from achievements.models import Achievement
+from helpers.test_helpers import create_body
 
-# Create your tests here.
+class AchievementTestCae(APITestCase):
+    """Check if we can create and verify achievement requests."""
+
+    def setUp(self):
+        # Fake authenticate
+        self.user = get_new_user()
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+        # A different user
+        self.user_2 = get_new_user()
+
+        # Dummy bodiews
+        self.body_1 = create_body()
+        self.body_2 = create_body()
+
+    def test_get_achievement(self):
+        """Test retrieve method of achievement viewset."""
+
+        achievement_1 = Achievement.objects.create(
+            description="TEST ACHIEVEMENT", body=self.body_1, user=self.user.profile)
+
+        url = '/api/achievements/%s' % achievement_1.id
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['description'], achievement_1.description)
