dbefa720b401d3b0091abb1c70c9b689517aaee9
Varun Patil <varun@mercari.com>
2019-07-22 04:57:10 +0900

    refactor(users): Rearrange fields in model

--- users/models.py
+++ users/models.py
@@ -20,45 +20,51 @@ class UserProfile(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
 
+    # Linked Django User object
     user = models.OneToOneField(
         User, related_name='profile', on_delete=models.CASCADE, null=True, blank=True)
 
+    # Basic info from SSO
     name = models.CharField(max_length=50, blank=True)
     roll_no = models.CharField(max_length=30, null=True, blank=True)
     ldap_id = models.CharField(max_length=50, null=True, blank=True)
     profile_pic = models.URLField(null=True, blank=True)
-    fcm_id = models.CharField(max_length=200, null=True, blank=True)
 
-    followed_bodies = models.ManyToManyField('bodies.Body', related_name='followers', blank=True)
+    # Advanced info from SSO
     contact_no = models.CharField(max_length=30, null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
-    about = models.TextField(blank=True, null=True)
-    website_url = models.URLField(blank=True, null=True)
     department = models.CharField(max_length=30, null=True, blank=True)
     department_name = models.CharField(max_length=200, null=True, blank=True)
     degree = models.CharField(max_length=200, null=True, blank=True)
     degree_name = models.CharField(max_length=200, null=True, blank=True)
     join_year = models.CharField(max_length=5, null=True, blank=True)
     graduation_year = models.CharField(max_length=5, null=True, blank=True)
+    hostel = models.CharField(max_length=100, null=True, blank=True)
+    room = models.CharField(max_length=30, null=True, blank=True)
 
+    # InstiApp feature fields
+    followed_bodies = models.ManyToManyField('bodies.Body', related_name='followers', blank=True)
+
+    # InstiApp roles
     roles = models.ManyToManyField('roles.BodyRole', related_name='users', blank=True)
     former_roles = models.ManyToManyField(
         'roles.BodyRole', related_name='former_users', blank=True, through='UserFormerRole')
     institute_roles = models.ManyToManyField(
         'roles.InstituteRole', related_name='users', blank=True)
-    hostel = models.CharField(max_length=100, null=True, blank=True)
-    room = models.CharField(max_length=30, null=True, blank=True)
 
-    android_version = models.IntegerField(default=0)
-
+    # User exposed fields
     show_contact_no = models.BooleanField(default=True)
+    fcm_id = models.CharField(max_length=200, null=True, blank=True)
+    about = models.TextField(blank=True, null=True)
+    android_version = models.IntegerField(default=0)
+    website_url = models.URLField(blank=True, null=True)
 
     class Meta:
         verbose_name = "Profile"
         verbose_name_plural = "Profiles"
 
     class ExMeta:
-        user_editable = ('show_contact_no', 'fcm_id', 'about', 'android_version')
+        user_editable = ('show_contact_no', 'fcm_id', 'about', 'android_version', 'website_url')
 
     def __str__(self):
         return self.name
