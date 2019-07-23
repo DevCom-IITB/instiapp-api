ed0ecd112f0b0b8d7d7829b568420ba361f37d86
Varun Patil <varun@mercari.com>
2019-07-23 17:02:50 +0900

    fix(fts): Index only valid objects

--- other/management/commands/reconstruct-search.py
+++ other/management/commands/reconstruct-search.py
@@ -1,13 +1,16 @@
 """Reconstruct all search indices."""
+from datetime import timedelta
 from asonic import Client as SonicClient
 from asonic.enums import Channels as SonicChannels
 from django.conf import settings
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 from other.search import push, index_pair, consolidate
 from other.asyncio_run import run_sync
 
 from bodies.models import Body
 from events.models import Event
+from events.prioritizer import get_fresh_events
 from users.models import UserProfile
 from placements.models import BlogEntry
 from news.models import NewsEntry
@@ -35,9 +38,20 @@ class Command(BaseCommand):
         if not settings.USE_SONIC:
             return
 
-        for model in (Body, Event, UserProfile, BlogEntry, NewsEntry):
-            # Refresh all objects
-            run_sync(refresh(model.objects.all()))
+        # Refresh all bodies and news
+        run_sync(refresh(Body.objects.all()))
+        run_sync(refresh(NewsEntry.objects.all()))
+
+        # Refresh events younger than two years
+        run_sync(refresh(get_fresh_events(Event.objects, delta=600)))
+
+        # Refresh entries younger than two years
+        placement = BlogEntry.objects.filter(
+            published__gte=timezone.now() - timedelta(days=600))
+        run_sync(refresh(placement))
+
+        # Refresh active users
+        run_sync(refresh(UserProfile.objects.filter(active=True)))
 
         # Re-consolidate
         run_sync(consolidate())
