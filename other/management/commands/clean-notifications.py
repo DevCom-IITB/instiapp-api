483a8cf90278790ac6114f34bf9dea8595df9818
E Aakash <09e.aakash@gmail.com>
2020-12-28 13:58:07 +0530

    feat(notifications): fix old notifications dump logic
    - path argument -> file argument
    - dump one notification object per line to given file as json object

--- other/management/commands/clean-notifications.py
+++ other/management/commands/clean-notifications.py
@@ -10,14 +10,16 @@ class Command(BaseCommand):
     help = 'Clean up old notifications (older than 3 months)'
 
     def add_arguments(self, parser):
-        parser.add_argument('path', type=str, help='Give directory path for dumping old notifs without end slash')
+        parser.add_argument('file', type=str, help='File path for dumping old notifs')
 
     def handle(self, *args, **options):
-        file_path = options['path'] + '/' + str(now().date()) + '.txt'
+        file_path = options['file']
         queryset = Notification.objects.filter(timestamp__lte=now() - timedelta(days=90))
         print('Dumping and Cleaning up %s old notifications' % queryset.count())
         if queryset.count() > 0:
-            data = list(queryset.values())
-            with open(file_path) as f:
-                json.dump(data, f, ensure_ascii=False, default=str)
+            notif_list = list(queryset.values())
+            with open(file_path, 'a') as f:
+                for notif in notif_list:
+                    json.dump(notif, f, ensure_ascii=False, default=str)
+                    print("", file=f, end='\n')
             queryset.delete()
