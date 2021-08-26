7a35312f86dd663da09bee03a088c97e1065ef8e
dev9318 <dev.moxaj@gmail.com>
2021-08-27 00:41:23 +0530

    add model for external

---
+++ external/models.py
@@ -0,0 +1,25 @@
+"""Models for external opportunities."""
+from uuid import uuid4
+from django.db import models
+from django.utils.timezone import now
+
+class BlogEntry(models.Model):
+    """A single entry on the placements blog."""
+
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    guid = models.CharField(max_length=200, blank=True)
+    title = models.CharField(max_length=300, blank=True)
+    content = models.TextField(blank=True)
+    link = models.CharField(max_length=200, blank=True)
+    published = models.DateTimeField(default=now)
+    
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "External Blog Entry"
+        verbose_name_plural = "External Blog Entries"
+        ordering = ("-published",)
+        indexes = [
+            models.Index(fields=['guid', ]),
+        ]
