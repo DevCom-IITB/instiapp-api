9105210a34e68bc7a6e7e8e484b3348b49361cde
dev9318 <dev.moxaj@gmail.com>
2021-12-06 17:42:24 +0530

    add chore to extract queries from sheet

--- querybot/documents.py
+++ querybot/documents.py
@@ -19,14 +19,3 @@ class QueryDocument(Document):
             'sub_category',
             'sub_sub_category'
         ]
-
-        # Ignore auto updating of Elasticsearch when a model is saved
-        # or deleted:
-        # ignore_signals = True
-
-        # Don't perform an index refresh after every update (overrides global setting):
-        # auto_refresh = False
-
-        # Paginate the django queryset used to populate the index with the specified size
-        # (by default it uses the database driver's default setting)
-        # queryset_pagination = 5000
