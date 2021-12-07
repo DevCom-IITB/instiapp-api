166188a1e323e96098a91a9ace1237fcb0952e1b
dev9318 <dev.moxaj@gmail.com>
2021-12-07 23:34:48 +0530

    add an elasticsearch flag to settings

--- querybot/helpers.py
+++ querybot/helpers.py
@@ -1,28 +1,32 @@
 from elasticsearch_dsl import Q
+from django.conf import settings
 from querybot.models import Query
 from .documents import QueryDocument
 
 def query_search(query, categories):
-    if query == '' and categories[0] == '':
-        queryset = Query.objects.all()
-        return queryset
 
-    category_dic = {}
-    querydic = {"match": {"question": {"query": query, "fuzziness": "AUTO"}}}
-    if categories[0] == '':
-        res = QueryDocument.search().query(Q(querydic))[:20]
-    elif query == '':
-        category_dic = [Q('match', category=category_id) for category_id in categories]
-        query = category_dic.pop()
-        for x in category_dic:
-            query |= x
-        res = QueryDocument.search().query(query)
-    else:
-        category_dic = [Q('match', category=category_id) for category_id in categories]
-        query = category_dic.pop()
-        for x in category_dic:
-            query |= x
-        res = QueryDocument.search().query(query)
-        res = res.query(Q(querydic))
-    queryset = res.to_queryset()
-    return queryset[0:20]
+    if settings.USE_ELASTIC:
+        querydic = {"match": {"question": {"query": query, "fuzziness": "AUTO"}}}
+        if query == '' and categories[0] == '':
+            queryset = Query.objects.all()
+        elif categories[0] == '':
+            res = QueryDocument.search().query(Q(querydic))[:20]
+            queryset = res.to_queryset()
+        elif query == '':
+            category_dic = [Q('match', category=category_id) for category_id in categories]
+            query = category_dic.pop()
+            for x in category_dic:
+                query |= x
+            res = QueryDocument.search().query(query)
+            queryset = res.to_queryset()
+        else:
+            category_dic = [Q('match', category=category_id) for category_id in categories]
+            query = category_dic.pop()
+            for x in category_dic:
+                query |= x
+            res = QueryDocument.search().query(query)
+            res = res.query(Q(querydic))[:20]
+            queryset = res.to_queryset()
+        return queryset
+    queryset = Query.objects.all()
+    return queryset
