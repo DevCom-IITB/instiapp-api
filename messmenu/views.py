f77a2ef4488c7edd79c434c66b08c9d45f649fee
Palash Mittal <pmittal00@gmail.com>
2022-01-08 21:07:06 +0530

    Added mess calendar feature

--- messmenu/views.py
+++ messmenu/views.py
@@ -3,6 +3,10 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from messmenu.models import Hostel
 from messmenu.serializers import HostelSerializer
+from roles.helpers import login_required_ajax
+import requests
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 @api_view(['GET', ])
 def get_mess(request):
@@ -10,3 +14,60 @@ def get_mess(request):
     queryset = Hostel.objects.all()
     queryset = HostelSerializer.setup_eager_loading(queryset)
     return Response(HostelSerializer(queryset, many=True).data)
+
+@api_view(['GET', ])
+@login_required_ajax
+def getUserMess(request):
+    """Get mess status for a user"""
+    reqData = request.data
+    user = request.user.profile
+    rollno = user.rollno
+    start = reqData['start']
+    end = reqData['end']
+    
+    start = datetime.strptime(start, '%Y-%m-%d %H:%M:%S')
+    end = datetime.strptime(end, '%Y-%m-%d %H:%M:%S')
+    
+    curr = start
+    
+    items = []
+    
+    while curr<=end:
+        res = requests.get(""+"/api/get_details/", {"roll":rollno, "year":curr.year, "month":curr.month})
+        if res.status_code!=200:
+            print("Error in getting details")
+            return Response({"error":"Error in getting mess calendar"})
+        
+        data = res.json()
+        details = data["details"]
+        
+        for d in details:
+            k = binaryDecode(d)
+            mealnum = k["meal"]
+            if(mealnum == "000"):
+                title = "Breakfast"
+            elif(mealnum == "001"):
+                title = "Lunch"
+            elif(mealnum == "010"):
+                title = "Snacks"
+            elif(mealnum == "011"):
+                title = "Dinner"
+            else:
+                title = "Error"
+            
+            date = datetime(curr.year, curr.month, k["day"], k["time"]//60, k["time"]%60)
+            hostel = k["hostel"]
+            items.append({"title":title, "datetime":date, "hostel":hostel})
+            
+        curr = curr + relativedelta(months=1)
+    
+    return Response({"items":items})    
+    
+
+def binaryDecode(x):
+    b_x = "{0:b}".format(int(x))
+    day = int(b_x[len(b_x)-5:len(b_x)],2)
+    meal = b_x[len(b_x)-8:len(b_x)-5]
+    time = int(b_x[len(b_x)-19:len(b_x)-8],2)
+    hostel = int(b_x[0:len(b_x)-19],2)
+    return {'hostel':hostel, 'time':time, 'meal':meal, 'day':day}
