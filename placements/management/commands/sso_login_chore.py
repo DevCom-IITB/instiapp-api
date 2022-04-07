88d3b5bf228ae3b8e497099d19ebebacda2b9c55
dev9318 <dev.moxaj@gmail.com>
2022-04-07 18:01:01 +0530

    add sso login support to placement blog scrapper

---
+++ placements/management/commands/sso_login_chore.py
@@ -0,0 +1,33 @@
+import pickle
+from requests import Session
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+from notifications.signals import notify
+import pyotp
+
+class Command(BaseCommand):
+    help = 'Updates the placement blog database'
+
+    def handle(self, *args, **options):
+        # """Run the chore."""
+        
+        s = Session()
+
+        init_response = s.get('https://sso.iitb.ac.in/login')
+
+        if not settings.LDAP_USERNAME or not settings.LDAP_PASSWORD:
+            raise CommandError('SSO LOGIN CHORE FAILED: NO CREDENTIALS PROVIDED')
+
+        s.post('https://sso.iitb.ac.in/login', cookies=init_response.cookies, data={
+            'username': settings.LDAP_USERNAME,
+            'password': settings.LDAP_PASSWORD,
+            'remember': 'on',
+            'redir': '/',
+            'totp': int(pyotp.TOTP(settings.SSO_TOTP_TOKEN).now())
+        }, allow_redirects=True)
+
+        f = open('session.obj', 'wb')
+        pickle.dump(obj=s, file=f)
+        f.close()
+
+        self.stdout.write(self.style.SUCCESS('SSO Login Chore completed successfully'))
