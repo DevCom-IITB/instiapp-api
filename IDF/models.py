6694e4db94b5bed27d3581a0912192e6e6f80d97
SanskarGosavi2003 <210100132@iitb.ac.in>
2022-06-12 00:48:21 +0530

    Created post model

--- IDF/models.py
+++ IDF/models.py
@@ -1,14 +1,16 @@
 from uuid import uuid4
 from django.db import models
 from helpers.misc import get_url_friendly
-class idf_Post(models.Model):
-    id=models.UUIDField(primary_key=True, editable=False, default=uuid4)
-    content=models.TextField(blank=True) #this will store the content of the post
-    image_url = models.URLField(blank=True, null=True) #for the image of the post
-    notification=models.BooleanField(default=True) #whenever a new post is created the notification field will will be initialized with the default value true thus sending the user a notification
-    posted_by=models.ForeignKey('users.UserProfile', null=True, blank=True,
-                                   on_delete=models.SET_NULL, related_name='posted')#to store info of the user posting
-    website_url= models.URLField(blank=True, null=True)#to save any links by the user in the post
-    #reactions need emojis
- 
-    
+
+class IDF_Post_Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    content = models.TextField(blank=True,null=True)
+    image_url = models.URLField(blank=True, null=True)
+    website_url = models.URLField(blank=True, null=True)
+    reaction = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    threadRank = models.IntegerField(default=1)
+    comments = models.ForeignKey("self", null=True, related_name="IDF_Post_Comment")
+    gif = models.FileField(upload_to = 'Videos/', blank = True)
+    tag_checkIn = models.URLField(blank=True, null=True)
+
