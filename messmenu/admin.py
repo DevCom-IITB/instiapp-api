3342f44ccfe03cf8fbdc985564900e427a297ad8
dev9318 <dev.moxaj@gmail.com>
2022-06-19 00:54:05 +0530

    add more functionality to admin pages

--- messmenu/admin.py
+++ messmenu/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from messmenu.models import Hostel, MessCalEvent
 from messmenu.models import MenuEntry
 
+class HostelAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ['name']
+
+
+class MessCalEventAdmin(admin.ModelAdmin):
+    list_filter = ('datetime', 'hostel',)
+    list_display = ('user', 'hostel', 'title',)    
+
 # Register your models here.
-admin.site.register(Hostel)
+admin.site.register(Hostel, HostelAdmin)
 admin.site.register(MenuEntry)
-admin.site.register(MessCalEvent)
+admin.site.register(MessCalEvent, MessCalEventAdmin)
--- placements/admin.py
+++ placements/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from placements.models import BlogEntry
 
-admin.site.register(BlogEntry)
+class BlogEntryAdmin(admin.ModelAdmin):
+    list_filter = ('published',)
+    list_display = ('title', 'published')
+    search_fields = ['title']
+
+
+admin.site.register(BlogEntry, BlogEntryAdmin)
