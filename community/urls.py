4ca0c73b5d45eb764ba372305d7371d7718bfa8d
SanskarGosavi2003 <210100132@iitb.ac.in>
2022-07-13 16:51:49 +0530

    corrected create and update functions

--- community/urls.py
+++ community/urls.py
@@ -12,18 +12,11 @@ urlpatterns = [
     })),#viewing a particular community
 
     path('communityposts', PostViewSet.as_view({
-        'get': 'list','post': 'create_post','put': 'update_post','delete': 'destroy'
+        'get': 'list','post': 'create','put': 'update','delete': 'destroy'
     })),#viewing the list of posts in their minimum view
 
     path('communityposts/<pk>', PostViewSet.as_view({
         'get': 'retrieve_full','delete': 'destroy'
     })),#to get the full view of a post
 
-    path('communitypostcomments', PostViewSet.as_view({
-        'get':'list','post': 'create_comment','put': 'update_comment', 'delete': 'destroy_comment'
-    })),#updating a comment and creating one
-
-    path('communitiespostcomments/<pk>', PostViewSet.as_view({
-        'delete': 'destroy'
-    })),#to get the full view of a post
 ]
