f8d4ab70df08bd22d3a1cdf1fea4d2cb651085dc
Palash Mittal <pmittal00@gmail.com>
2022-10-03 17:06:54 +0530

    fix(community): add/rem null and blank arguments

--- community/models.py
+++ community/models.py
@@ -8,17 +8,17 @@ class Community(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     str_id = models.CharField(max_length=58, editable=False, null=True)
     name = models.CharField(max_length=100)
-    about = models.TextField()
-    description = models.TextField()
+    about = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     logo_image = models.URLField(blank=True, null=True)
     cover_image = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    body = models.ForeignKey('bodies.Body', on_delete=models.CASCADE, related_name='community_body', null=True)
+    body = models.ForeignKey('bodies.Body', on_delete=models.CASCADE, related_name='community_body')
     followers = models.IntegerField(default=0)
 
     def __str__(self):
-        return str(self.id)
+        return str(self.name)
 
     def save(self, *args, **kwargs):        # pylint: disable=W0222
         self.str_id = get_url_friendly(self.name) + "-" + str(self.id)[:8]
@@ -34,18 +34,18 @@ class CommunityPost(models.Model):
     str_id = models.CharField(max_length=58, editable=False, null=True)
     time_of_creation = models.DateTimeField(auto_now_add=True)
     time_of_modification = models.DateTimeField(auto_now=True)
-    content = models.TextField(blank=True)
+    content = models.TextField(null=True, blank=True)
     image_url = models.TextField(blank=True, null=True)
-    reported_by=models.ManyToManyField('users.UserProfile', related_name='posts_reported', blank=True)
+    reported_by = models.ManyToManyField('users.UserProfile', related_name='posts_reported', blank=True)
     reacted_by = models.ManyToManyField(
         'users.UserProfile', through='CommunityPostUserReaction',
         related_name='communitypost_reaction', blank=True)
     view_count = models.IntegerField(default=0)
     featured = models.BooleanField(default=False)
     deleted = models.BooleanField(default=False)
-    community = models.ForeignKey(Community, on_delete=models.CASCADE, related_name='posts', null=True, blank=True)
-    ignored = models.BooleanField(default= False)
-    thread_rank = models.IntegerField(default=1, null=True)
+    community = models.ForeignKey(Community, on_delete=models.CASCADE, related_name='posts')
+    ignored = models.BooleanField(default=False)
+    thread_rank = models.IntegerField(default=1, null=True, blank=True)
     parent = models.ForeignKey("self", blank=True, null=True,
                                related_name="comments", on_delete=models.CASCADE)
     # comments = models.ManyToManyField(
@@ -55,9 +55,8 @@ class CommunityPost(models.Model):
     tag_body = models.ManyToManyField('bodies.Body', related_name="Tagged_Body", blank=True)
     tag_location = models.ManyToManyField('locations.Location', related_name='communitypost_tagloc', blank=True)
     followed_by = models.ManyToManyField("users.UserProfile", related_name="communitypost_followers", blank=True)
-    anonymous = models.BooleanField(default=False,blank=True)
-    posted_by = models.ForeignKey('users.UserProfile', null=True, blank=True,
-                                  on_delete=models.SET_NULL, related_name='communitypost_postedby')
+    anonymous = models.BooleanField(default=False, blank=True)
+    posted_by = models.ForeignKey('users.UserProfile', on_delete=models.CASCADE, related_name='communitypost_postedby')
     interests = models.ManyToManyField('achievements.Interest', related_name='communitypost_interest', blank=True)
 
     """ Status
@@ -75,7 +74,7 @@ class CommunityPost(models.Model):
             if self.reported_by.count() > 1:
                 self.status = 3
         super().save(*args, **kwargs)
-        
+
     def __str__(self) -> str:
         return self.content[:100]
 
@@ -96,10 +95,8 @@ class CommunityPostUserReaction(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     time_of_creation = models.DateTimeField(auto_now_add=True)
 
-    user = models.ForeignKey('users.UserProfile', on_delete=models.CASCADE,
-                             default=uuid4, related_name='ucpr')
-    communitypost = models.ForeignKey(CommunityPost, on_delete=models.CASCADE,
-                                      default=uuid4, related_name='ucpr')
+    user = models.ForeignKey('users.UserProfile', on_delete=models.CASCADE, related_name='ucpr')
+    communitypost = models.ForeignKey(CommunityPost, on_delete=models.CASCADE, related_name='ucpr')
     reaction = models.IntegerField(default=0)
 
     class Meta:
