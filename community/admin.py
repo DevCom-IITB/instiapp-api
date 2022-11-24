45a96bd8eca332853da3147f8429534611305d86
Palash Mittal <pmittal00@gmail.com>
2022-11-24 16:32:45 +0530

    fix(community): fix raw_id_fields

--- community/admin.py
+++ community/admin.py
@@ -3,9 +3,10 @@ from community.models import Community, CommunityPost, CommunityPostUserReaction
 
 class CommunityPostAdmin(admin.ModelAdmin):
     search_fields = ['content', 'community__name', 'posted_by__name']
-    list_display = ('content', 'community__name', 'thread_rank', 'posted_by__name')
+    list_display = ('content', 'community', 'thread_rank', 'posted_by')
     list_filter = ('community__name', 'thread_rank')
-    raw_id_fields = ('reported_by', 'reacted_by', 'followed_by', 'interests', 'tag_user', 'tag_body', 'tag_location')
+    raw_id_fields = ('reported_by', 'posted_by', 'community', 'parent', 'reacted_by',
+                     'followed_by', 'interests', 'tag_user', 'tag_body', 'tag_location')
 
 
 admin.site.register(Community)
