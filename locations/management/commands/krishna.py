4fd1703fbf45594e4af77b11953a20d9725d6765
Atharv140903 <atharvkurde2003@gmail.com>
2023-01-11 18:06:06 +0530

    navigation

--- locations/management/commands/krishna.py
+++ locations/management/commands/krishna.py
@@ -10,6 +10,8 @@ from external.models import ExternalBlogEntry
 from bodies.models import Body
 from helpers.misc import table_to_markdown
 
+from locations.models import Location, LocationLocationDistance
+
 
 # class ProfileFetcher():
 #     """Helper to get dictionary of profiles efficiently."""
@@ -34,7 +36,27 @@ def handle_entry(entry):
     for x in adj_list:
         for y in adj_list[x]:
              adj_list[x][y] = abs(0.001*((coordinates[x][0]^2) - (coordinates[y][0]^2) + (coordinates[x][1]^2) - (coordinates[y][1]^2)))
-       
+
+    i=0
+    loc_list = []
+    for p in coordinates:
+        loc =  Location.objects.get_or_create(pixel_x= p[0], pixel_y= p[1], name="Node"+i)
+        loc_list.append(loc)
+        i+=1
+
+    for i in range(0, len(coordinates)):
+        loc1 = loc_list[i]
+        for loc2_ind, dist in adj_list[i]:
+            loc2 = loc_list[loc2_ind]
+            lld = LocationLocationDistance.objects.filter(location1=loc1, location2=loc2).first()
+            if not lld:
+                LocationLocationDistance.objects.create(
+                    location1=loc1, location2=loc2, distance=dist)
+            else:
+                lld.distance = dist
+                lld.save()
+
+
 
 
 
