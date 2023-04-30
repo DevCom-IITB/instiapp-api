e6b10b2f01438920f707c2ad18c9864ff2a05c1c
VIBR0X <44766005+vedant-github@users.noreply.github.com>
2023-04-30 12:33:08 +0530

    buy&sell(views): removed results_per_page form queryset

--- buyandsell/views.py
+++ buyandsell/views.py
@@ -83,7 +83,7 @@ class BuyAndSellViewSet(viewsets.ModelViewSet):
         queryset = query_search(request, 3, queryset, ['name', 'description'], 'buyandsell')
         queryset = self.category_filter(request, queryset)
         queryset = self.seller_filter(request, queryset)
-        queryset = query_from_num(request, self.RESULTS_PER_PAGE, queryset)
+        # queryset = query_from_num(request, self.RESULTS_PER_PAGE, queryset)
         data = ProductSerializer(queryset, many=True).data
         return Response(data)
     def get_contact_details(userpro:UserProfile):
