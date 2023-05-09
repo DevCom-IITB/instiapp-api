372d3b69638b5a760ae9e78233b1b552a2a72b07
VIBR0X <44766005+vedant-github@users.noreply.github.com>
2023-05-09 20:27:34 +0530

    Buy&Sell: fix lint in models, admin, urls

--- buyandsell/urls.py
+++ buyandsell/urls.py
@@ -3,18 +3,18 @@ from django.urls import path
 from buyandsell.views import BuyAndSellViewSet
 
 urlpatterns = [
-    #use list for search too to make use of pagination.
+    # use list for search too to make use of pagination.
     path('buy/products', BuyAndSellViewSet.as_view({
         'get': 'list', 'post': 'create'
     })),
 
     path('buy/products/<pk>', BuyAndSellViewSet.as_view({
-        'get':'retrieve','put': 'update', 'delete': 'destroy'
+        'get': 'retrieve', 'put': 'update', 'delete': 'destroy'
     })),
     path('buy/report/<pk>', BuyAndSellViewSet.as_view({
-        'post':'report'
+        'post': 'report'
     })),
     path('buy/categories', BuyAndSellViewSet.as_view({
-        'get':'get_categories'
+        'get': 'get_categories'
     }))
 ]
