6f503c4d1e3c84c9bb6f60e4d08c8d1fc4d3ecf3
Krishna-Baldwa <krishnabaldwa1101.com>
2023-05-11 22:56:23 +0530

    locations were updated

--- backend/settings.py
+++ backend/settings.py
@@ -17,8 +17,8 @@ ALLOWED_HOSTS = ['*']
 # SSO Config
 SSO_TOKEN_URL = 'https://gymkhana.iitb.ac.in/sso/oauth/token/'
 SSO_PROFILE_URL = 'https://gymkhana.iitb.ac.in/sso/user/api/user/?fields=first_name,last_name,type,profile_picture,sex,username,email,program,contacts,insti_address,secondary_emails,mobile,roll_number'
-SSO_CLIENT_ID = '5jyMJufq0Vk0aDlj9Hnudsj84UfbFZlYRUnn02Xd'
-SSO_CLIENT_ID_SECRET_BASE64 = 'NWp5TUp1ZnEwVmswYURsajlIbnVkc2o4NFVmYkZabFlSVW5uMDJYZDo2RTN5S3g4OU5XWVFxcWFJaUdwcE9RdFF4TmZxSkxFYXhkcDVXb2s2VzZsdGxDQWpPWmM3SWJrVXVpUXdnaTZNaGVoUU1LWnVTelFUMU1EOHBockhhVThvVUk5S1hYZ2NHMG51UE03Wkk3V0xoWjYydG5OZFU4Uk02TTJXaXdJZw=='
+SSO_CLIENT_ID = 'cL7imsMpCgneHK6QDE71szz7elWKqVLZHBONaPJA'
+SSO_CLIENT_ID_SECRET_BASE64 = 'Y0w3aW1zTXBDZ25lSEs2UURFNzFzeno3ZWxXS3FWTFpIQk9OYVBKQTpuWEFDa1duMGpRaGlBdFZnVjd3UkpibUNqemEzS0EzMXlyYU8zN1FxMm9sbXJNWXFMeTVWZlpFUmt3eDU1czI2RGcyRFQ4N2tLTUdMaU9BUExqR0pKZDhQNlNYUVJJUWN6RFNVclduNWVuaUNkQUI3MXAwV2NjRDd3WHRuQlB4YQ===='
 
 # Password Login
 SSO_DEFAULT_REDIR = 'https://insti.app/login'
