408cee2dbfe0a3098150b8d3089d9536f19e2de5
VIBR0X <44766005+vedant-github@users.noreply.github.com>
2023-07-13 11:32:23 +0530

    change settings.pyto resolve CORS error

--- backend/settings_base.py
+++ backend/settings_base.py
@@ -210,10 +210,6 @@ CELERY_DELAY = 0
 
 # DEFAULT_FROM_EMAIL = 'webmaster@localhost'
 
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'keepwatch354@gmail.com'
-EMAIL_HOST_PASSWORD = 'WalterPinkman'
 COMPLAINT_AUTO_SUBSCRIBE = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
