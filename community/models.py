92df7e38a3d691257b73900c1b5e15d2417fc15c
Palash Mittal <pmittal00@gmail.com>
2023-07-24 23:40:36 +0530

    fix(users): remove field followed_communities

--- community/models.py
+++ community/models.py
@@ -23,13 +23,6 @@ class Community(models.Model):
     def save(self, *args, **kwargs):        # pylint: disable=W0222
         self.str_id = get_url_friendly(self.name) + "-" + str(self.id)[:8]
         super().save(*args, **kwargs)
-    
-    @staticmethod
-    def pre_save(self):
-        followers = UserProfile.objects.filter(followed_communities__id=self.id)
-        if followers:
-            self.followers = followers.count()
-
 
     class Meta:
         verbose_name = "Community"
