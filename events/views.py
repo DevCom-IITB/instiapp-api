847f4a388410ab898878c730a3a2005f85a7d36e
Jatin Singhal <jatinsinghal@Jatins-MacBook-Air-2.local>
2023-12-30 18:26:36 +0530

    Removed my mail credentials

--- events/views.py
+++ events/views.py
@@ -216,7 +216,7 @@ class EventMailVerificationViewSet(viewsets.ViewSet):
             if user_has_VerE_permission:
                 subject = event.description
                 message = event.longdescription
-                recipient_list = ['22b1277@iitb.ac.in']
+                recipient_list = ['']
                 try:
                     send_mail(subject, message, EMAIL_HOST_USER, recipient_list, fail_silently=False)
                     event.email_verified = True
