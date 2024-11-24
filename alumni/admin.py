b6086c081c8f2186a78b7b4a9c5e6e8e4ed3e984
HariRags <harigovindraghunath@gmail.com>
2024-11-24 13:13:33 +0530

    added alumni search

--- alumni/admin.py
+++ alumni/admin.py
@@ -4,6 +4,7 @@ from alumni.models import AlumniUser
 
 class AlumniUserAdmin(admin.ModelAdmin):
     list_display = ("ldap", "keyStored", "timeLoginRequest")
-
+    search_fields = ["ldap"]
+i
 
 admin.site.register(AlumniUser, AlumniUserAdmin)
