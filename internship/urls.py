0566a0e3de99e172d2d90aaf507d15dfb04a1a3e
dhruvparsewar <parsewardhruv@gmail.com>
2026-06-29 19:38:51 +0530

    feat: Added Blog list views

--- internship/urls.py
+++ internship/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ThreadListView, ThreadDetailView
+from .views import ThreadListView, ThreadDetailView, BlogPostListView, LatestCompanyPostListView
 
 urlpatterns = [
     path('threads/', ThreadListView.as_view(), name='thread-list'),
     path('threads/<slug:company_slug>/', ThreadDetailView.as_view(), name='thread-detail'),
+    path('posts/', BlogPostListView.as_view(), name='post-list'),
+    path('posts/latest/', LatestCompanyPostListView.as_view(), name='latest-company-posts'),
 ]
--- internship/views.py
+++ internship/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
-from .models import CompanyThread
-from .serializers import CompanyThreadSerializer, CompleteThreadSerializer
+from .models import CompanyThread, BlogPost
+from .serializers import CompanyThreadSerializer, CompleteThreadSerializer, BlogPostSerializer
+from django.db.models import OuterRef, Subquery
 
 class ThreadListView(generics.ListAPIView):
 
@@ -15,3 +16,22 @@ class ThreadDetailView(generics.RetrieveAPIView):
     serializer_class = CompleteThreadSerializer
     
     lookup_field = 'company_slug'
+
+class BlogPostListView(generics.ListAPIView):
+
+    queryset = BlogPost.objects.select_related('extracted').order_by('-published')
+    serializer_class = BlogPostSerializer
+
+class LatestCompanyPostListView(generics.ListAPIView):
+    serializer_class = BlogPostSerializer
+
+    def get_queryset(self):
+
+        latest_post_subquery = BlogPost.objects.filter(
+            thread=OuterRef('thread')
+        ).order_by('-published').values('id')[:1]
+
+        #Filter the main BlogPost table to only include those exact IDs, 
+        return BlogPost.objects.filter(
+            id=Subquery(latest_post_subquery)
+        ).select_related('extracted').order_by('-published')
