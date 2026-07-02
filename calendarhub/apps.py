38f54fcbbd10c2327bbdbda13b3bc5021e3b18f0
Jithin <jithinjithi226@gmail.com>
2026-07-02 22:33:37 +0530

    implemented default subscription logic

--- calendarhub/apps.py
+++ calendarhub/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CalendarhubConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'calendarhub'
+    
+    def ready(self):
+        from calendarhub.tasks import signals  # noqa: F401
