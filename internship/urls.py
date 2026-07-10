229e99335729229da1cdeb6f4c0ac9b9d8d2a4d4
dhruvparsewar <parsewardhruv@gmail.com>
2026-07-10 13:17:38 +0530

    fix: minor changes in naming of api endpoints

--- internship/urls.py
+++ internship/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import ThreadListView, ThreadDetailView, BlogPostListView, LatestCompanyPostListView, FilterOptionsView
 
 urlpatterns = [
-    path('filters/', FilterOptionsView.as_view(), name='filter-options'),
-    path('threads/', ThreadListView.as_view(), name='thread-list'),
-    path('threads/<slug:company_slug>/', ThreadDetailView.as_view(), name='thread-detail'),
-    path('posts/', BlogPostListView.as_view(), name='post-list'),
-    path('posts/latest/', LatestCompanyPostListView.as_view(), name='latest-company-posts'),
+    path('internship/filters/', FilterOptionsView.as_view(), name='filter-options'),
+    path('internship/threads/', ThreadListView.as_view(), name='thread-list'),
+    path('internship/threads/<slug:company_slug>/', ThreadDetailView.as_view(), name='thread-detail'),
+    path('internship/posts/', BlogPostListView.as_view(), name='post-list'),
+    path('internship/posts/latest/', LatestCompanyPostListView.as_view(), name='latest-company-posts'),
 ]
--- placementblogs/urls.py
+++ placementblogs/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import PlacementThreadListView, PlacementThreadDetailView, PlacementBlogPostListView, PlacementLatestCompanyPostListView, FilterOptionsView
 
 urlpatterns = [
-    path('filters/', FilterOptionsView.as_view(), name='filter-options'),
-    path('threads/', PlacementThreadListView.as_view(), name='placement-thread-list'),
-    path('threads/<slug:company_slug>/', PlacementThreadDetailView.as_view(), name='placement-thread-detail'),
-    path('posts/', PlacementBlogPostListView.as_view(), name='placement-post-list'),
-    path('posts/latest/', PlacementLatestCompanyPostListView.as_view(), name='placement-latest-company-posts'),
+    path('placementblogs/filters/', FilterOptionsView.as_view(), name='filter-options'),
+    path('placementblogs/threads/', PlacementThreadListView.as_view(), name='placement-thread-list'),
+    path('placementblogs/threads/<slug:company_slug>/', PlacementThreadDetailView.as_view(), name='placement-thread-detail'),
+    path('placementblogs/posts/', PlacementBlogPostListView.as_view(), name='placement-post-list'),
+    path('placementblogs/posts/latest/', PlacementLatestCompanyPostListView.as_view(), name='placement-latest-company-posts'),
 ]
